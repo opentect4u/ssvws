24b70525a80e73be5eb552b7a4870ba409b77166
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-08 18:00:58 +0530

    commit 08.10.2024

--- router/admin/adminuserRouter.js
+++ router/admin/adminuserRouter.js
@@ -29,7 +29,7 @@ adminuserRouter.post('/save_profile_web', async (req, res) => {
     res.send(res_dt)
 });
 
-adminuserRouter.post('/password_change', async (req, res) => {
+adminuserRouter.post('/password_change_user', async (req, res) => {
     var data = req.body, result;
     console.log(data,'pwd');
     const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
@@ -39,20 +39,30 @@ adminuserRouter.post('/password_change', async (req, res) => {
     whr = `emp_id='${data.emp_id}'`;
     var res_dt = await db_Select(select,table_name,whr,null)
 
-    if(res_dt.suc > 0 && res_dt.msg.length > 0) {
+    if(res_dt.suc > 0) {
+        if(res_dt.msg.length > 0){
           if (await bcrypt.compare(data.old_pwd, res_dt.msg[0].password)) {
-            var pass = bcrypt.hashSync(data.new_pwd, 10);
-            var table_name = "md_user",
-            fields = `password = '${pass}', modified_by='${data.emp_name}', modified_dt='${datetime}'`,
-            whr = `emp_id = '${data.emp_id}'`,
-            flag = 1;
-            var reset_pass = await db_Insert(table_name,fields,null,whr,flag)
-            result = reset_pass     
-      }else {
-       
+                var pass = bcrypt.hashSync(data.new_pwd, 10);
+                var table_name = "md_user",
+                fields = `password = '${pass}', modified_by='${data.emp_name}', modified_dt='${datetime}'`,
+                whr = `emp_id = '${data.emp_id}'`,
+                flag = 1;
+                var reset_pass = await db_Insert(table_name,fields,null,whr,flag);
+                res.send({ suc: 1, msg: "Password updated successfully" });
+        }else {
+        result = {
+            suc: 0,
+            msg: "Please check your userid or password",
+          };
+          res.send(result)
       }
     }else {
-
+        result = { suc: 2, msg: "No data found", dt: log_dt };
+        res.send(result)
+    }
+    }else {
+        result = { suc: 0, msg: log_dt.msg, dt: log_dt };
+        res.send(result)
     }
 });
     
