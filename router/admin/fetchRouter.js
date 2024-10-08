b582fbbabd23d53d7403bb51b3c8b992c690c75f
Tan <mondal.tanmoy@synergicsoftek.com>
2024-10-08 15:38:51 +0530

    commit 08.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -7,21 +7,22 @@ fetchRouter.get("/fetch_bmfwd_dtls_web", async (req, res) => {
     var data = req.query;
     console.log(data,'dd');
     
-    var select = 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
+    var select = data.prov_grp_code > 0 ? 'a.prov_grp_code, b.*' : 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
     table_name = 'td_grt_basic a, md_group b',
     whr = `a.prov_grp_code = b.group_code
     AND a.approval_status = 'S' ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''}`,
     order = null;
     var res_dt = await db_Select(select,table_name,whr,order);
 
-    if(res_dt.suc > 0){
+    if(res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0){
         var select = '*',
         table_name = 'td_grt_basic',
-        whr = `a.approval_status = 'S'`,
+        whr = `approval_status = 'S' AND prov_grp_code = ${data.prov_grp_code}`,
         order = null;
         var mem_dt = await db_Select(select,table_name,whr,order);
+        res_dt.msg[0]['memb_dt'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : []
     }
-    res.send(mem_dt)
+    res.send(res_dt)
 });
 
 module.exports = {fetchRouter}
