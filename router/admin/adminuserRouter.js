2514377c6302361f59113fdb23284053ca884604
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-08 11:34:18 +0530

    commit 08.10.2024

--- router/admin/adminuserRouter.js
+++ router/admin/adminuserRouter.js
@@ -10,7 +10,7 @@ adminuserRouter.post('/save_profile_web', async (req, res) => {
 
     var table_name = "md_employee",
     fields = `emp_name = '${data.emp_name}', branch_id = '${data.branch_id}', phone_home = '${data.phone_home}', 
-    phone_mobile = '${data.phone_mobile}', email = '${data.email}', gender = '${data.gender}', modified_by = '${data.user}', modified_dt = '${datetime}'`,
+    phone_mobile = '${data.phone_mobile}', email = '${data.email}', gender = '${data.gender}', modified_by = '${data.emp_name}', modified_dt = '${datetime}'`,
     values = null
     whr = `emp_id = '${data.emp_id}' AND branch_id = '${data.branch_id}'`,
     order = null;
