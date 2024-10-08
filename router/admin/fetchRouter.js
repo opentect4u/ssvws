656cade5b2b9b141a8fc6b6eb4e02e2ae12a996d
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-08 13:38:44 +0530

    commit 08.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -5,12 +5,13 @@ dateFormat = require('dateformat');
 
 fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
     var data = req.post;
-    // console.log(data,'dd');
+    console.log(data,'dd');
     
-    var select = 'a.*, b.group_name',
-    table_name = 'td_grt_basic a, md_group b',
+    var select = 'a.*, b.group_name, c.user_type',
+    table_name = 'td_grt_basic a, md_group b, md_user c',
     whr = `a.prov_grp_code = b.group_code
-    AND a.approval_status = 'S'`,
+    AND a.approval_status = 'S'
+    AND c.user_type = '${data.id}'`,
     order = null;
     var res_dt = await db_Select(select,table_name,whr,order);
     res.send(res_dt)
