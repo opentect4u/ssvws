8a9df6156fae67ea663444350ffec7ab16f69d38
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-17 12:17:18 +0530

    add approve api 17.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -8,11 +8,10 @@ fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
     var data = req.body;
     console.log(data,'dd');
     
-    if(data.id == 3){
     var select = data.prov_grp_code > 0 ? 'a.prov_grp_code, b.*' : 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
     table_name = 'td_grt_basic a, md_group b',
     whr = `a.prov_grp_code = b.group_code
-    AND a.approval_status = 'S' ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''}`,
+    AND a.approval_status = 'S' ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''} ${data.user_type == 2 && data.branch_code > 0 ? `branch_code = ${data.branch_code}` : ''}`,
     order = null;
     var res_dt = await db_Select(select,table_name,whr,order);
 
@@ -24,25 +23,6 @@ fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
         var mem_dt = await db_Select(select,table_name,whr,order);
         res_dt.msg[0]['memb_dt'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : []
     }
-}else {
-    var select = data.prov_grp_code > 0 ? 'a.prov_grp_code, b.*' : 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
-    table_name = 'td_grt_basic a, md_group b',
-    whr = `a.prov_grp_code = b.group_code
-    AND a.branch_code = b.branch_code
-    AND a.branch_code = '${data.branch_code}'
-    AND a.approval_status = 'S' ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''}`,
-    order = null;
-    var res_dt = await db_Select(select,table_name,whr,order);
-
-    if(res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0){
-        var select = '*',
-        table_name = 'td_grt_basic',
-        whr = `approval_status = 'S' AND prov_grp_code = ${data.prov_grp_code} AND branch_code = '${data.branch_code}'`,
-        order = null;
-        var mem_dt = await db_Select(select,table_name,whr,order);
-        res_dt.msg[0]['memb_dt'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : []
-    }
-}
     res.send(res_dt)
 });
 
