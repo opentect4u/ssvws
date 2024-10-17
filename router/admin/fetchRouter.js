f51548875e638f2c21ea3f01a445ec352be86af1
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-17 12:46:38 +0530

    add approve api 17.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -6,25 +6,30 @@ dateFormat = require('dateformat');
 
 fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
     var data = req.body;
-    console.log(data,'dd');
+    console.log(data, 'dd');
     
     var select = data.prov_grp_code > 0 ? 'a.prov_grp_code, b.*' : 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
     table_name = 'td_grt_basic a, md_group b',
     whr = `a.prov_grp_code = b.group_code
-    AND a.approval_status = 'S' ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''} ${data.user_type == 2 && data.branch_code > 0 ? `branch_code = ${data.branch_code}` : ''}`,
+           AND a.approval_status = 'S' 
+           ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''} 
+           ${data.user_type == 2 && data.branch_code > 0 ? `AND a.branch_code = ${data.branch_code}` : ''}`,
     order = null;
-    var res_dt = await db_Select(select,table_name,whr,order);
+    
+    var res_dt = await db_Select(select, table_name, whr, order);
 
-    if(res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0){
+    if (res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0) {
         var select = '*',
         table_name = 'td_grt_basic',
         whr = `approval_status = 'S' AND prov_grp_code = ${data.prov_grp_code}`,
         order = null;
-        var mem_dt = await db_Select(select,table_name,whr,order);
+        
+        var mem_dt = await db_Select(select, table_name, whr, order);
         res_dt.msg[0]['memb_dt'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : []
     }
-    res.send(res_dt)
+    res.send(res_dt);
 });
+
 
 fetchRouter.get("/fetch_occup_dt_web", async (req, res) => {
     var data = req.query;
