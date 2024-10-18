540fc5149842ab4eb9523ebc7393aff99ab07f53
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-18 10:45:46 +0530

    add approve api 18.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -47,7 +47,8 @@ fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
     if (res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0) {
         var select = '*',
             table_name = 'td_grt_basic',
-            whr = `approval_status ='${data.user_type == 2 ? 'U' : 'S'}' AND prov_grp_code = ${data.prov_grp_code}`,
+            // whr = `approval_status ='${data.user_type == 2 ? 'U' : 'S'}' AND prov_grp_code = ${data.prov_grp_code}`,
+            whr = `prov_grp_code = ${data.prov_grp_code}`,
             order = null;
         
         var mem_dt = await db_Select(select, table_name, whr, order);
