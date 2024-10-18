bba5482d3aaf9bd8255608d0286a2cda089378df
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-18 11:37:30 +0530

    add approve api 18.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -37,7 +37,7 @@ fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
     var select = data.prov_grp_code > 0 ? 'a.prov_grp_code, b.*' : 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
         table_name = 'td_grt_basic a, md_group b',
         whr = `a.prov_grp_code = b.group_code
-               ${data.user_type == 2 && data.branch_code > 0 ?  `AND a.approval_status = 'U'` :  `AND a.approval_status = 'S'`}'
+               ${data.user_type == 2 && data.branch_code > 0 ?  `AND a.approval_status = U` :  `AND a.approval_status = S`}'
                ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''} 
                ${data.user_type == 2 && data.branch_code > 0 ? `AND a.branch_code = ${data.branch_code}` : ''}`,
         order = null;
@@ -47,7 +47,7 @@ fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
     if (res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0) {
         var select = '*',
             table_name = 'td_grt_basic',
-            whr = `prov_grp_code = ${data.prov_grp_code} ${data.user_type == 3 ? `AND approval_status != 'S'` : `AND approval_status != ''`}`,
+            whr = `prov_grp_code = ${data.prov_grp_code} ${data.user_type == 3 ? `AND approval_status != S` : `AND approval_status != ''`}`,
             // whr = `prov_grp_code = ${data.prov_grp_code}`,
             order = null;
         
