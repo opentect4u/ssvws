9e35bb27aea8861fd145fe0410c1d65676da3603
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-18 10:39:06 +0530

    add approve api 18.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -4,29 +4,56 @@ const { edit_grp_web, edit_basic_dt_web, edit_occup_dt_web, edit_household_dt_we
 const fetchRouter = require('express').Router();
 dateFormat = require('dateformat');
 
+// fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
+//     var data = req.body;
+//     console.log(data, 'dd');
+    
+//     var select = data.prov_grp_code > 0 ? 'a.prov_grp_code, b.*' : 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
+//     table_name = 'td_grt_basic a, md_group b',
+//     whr = `a.prov_grp_code = b.group_code
+//            ${data.user_type == 2 && data.branch_code > 0 ? `AND a.approval_status = 'U' : a.approval_status = 'S'`}` 
+//            ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''} 
+//            ${data.user_type == 2 && data.branch_code > 0 ? `AND a.branch_code = ${data.branch_code}` : ''}`,
+//     order = null;
+    
+//     var res_dt = await db_Select(select, table_name, whr, order);
+
+//     if (res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0) {
+//         var select = '*',
+//         table_name = 'td_grt_basic',
+//         whr = `approval_status = 'S' AND prov_grp_code = ${data.prov_grp_code}`,
+//         order = null;
+        
+//         var mem_dt = await db_Select(select, table_name, whr, order);
+//         res_dt.msg[0]['memb_dt'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : []
+//     }
+//     res.send(res_dt);
+// });
+
 fetchRouter.post("/fetch_bmfwd_dtls_web", async (req, res) => {
     var data = req.body;
     console.log(data, 'dd');
     
     var select = data.prov_grp_code > 0 ? 'a.prov_grp_code, b.*' : 'DISTINCT a.prov_grp_code, b.group_name, b.group_type',
-    table_name = 'td_grt_basic a, md_group b',
-    whr = `a.prov_grp_code = b.group_code
-           AND a.approval_status = 'S' 
-           ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''} 
-           ${data.user_type == 2 && data.branch_code > 0 ? `AND a.branch_code = ${data.branch_code}` : ''}`,
-    order = null;
+        table_name = 'td_grt_basic a, md_group b',
+        whr = `a.prov_grp_code = b.group_code
+               AND a.approval_status = '${data.user_type == 2 && data.branch_code > 0 ? 'U' : 'S'}'
+               ${data.prov_grp_code > 0 ? `AND a.prov_grp_code = ${data.prov_grp_code}` : ''} 
+               ${data.user_type == 2 && data.branch_code > 0 ? `AND a.branch_code = ${data.branch_code}` : ''}`,
+        order = null;
     
     var res_dt = await db_Select(select, table_name, whr, order);
 
     if (res_dt.suc > 0 && res_dt.msg.length > 0 && data.prov_grp_code > 0) {
         var select = '*',
-        table_name = 'td_grt_basic',
-        whr = `approval_status = 'S' AND prov_grp_code = ${data.prov_grp_code}`,
-        order = null;
+            table_name = 'td_grt_basic',
+            whr = `approval_status = 'S' AND prov_grp_code = ${data.prov_grp_code}`,
+            order = null;
         
         var mem_dt = await db_Select(select, table_name, whr, order);
-        res_dt.msg[0]['memb_dt'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : []
+        res_dt.msg[0]['memb_dt'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : [];
     }
+    
     res.send(res_dt);
 });
 
