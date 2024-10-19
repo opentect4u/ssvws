df760769b8dd0f3ee3eef59928b8e178452b85e5
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-19 17:30:01 +0530

    add change grt save 19.10.2024

--- router/api/memberRouter.js
+++ router/api/memberRouter.js
@@ -9,7 +9,7 @@ memberRouter.post("/search_member", async (req, res) => {
     var data = req.body;
 
     // var select = "a.*, b.group_name",
-    var select = "a.*",
+    var select = "a.*,",
     // table_name = "md_member a LEFT JOIN md_group b ON a.branch_code = b.branch_code",
     table_name = "md_member a",
     // whr = `a.branch_code = '${data.branch_code}' 
@@ -21,6 +21,18 @@ memberRouter.post("/search_member", async (req, res) => {
 
 res.send(search_mem)
 });
+
+memberRouter.get("/get_grt_dtls", async (req, res) => {
+    var data = req.query;
+
+    var select = "a.form_no,a.grt_date,a.branch_code,a.prov_grp_code,a.member_code,a.approval_status,a.co_lat_val,a.co_long_val,a.co_gps_address,b.branch_name",
+    table_name = "td_grt_basic a LEFT JOIN md_branch b ON a.branch_code = b.branch_code",
+    whr = `a.member_code = '${data.member_code}'`,
+    order = null;
+    var dt_fetch = await db_Select(select,table_name,whr,order);
+
+    res.send(dt_fetch)
+})
 
 memberRouter.post("/edit_search_basic_dtls", async (req, res) => {
     var data = req.body;
