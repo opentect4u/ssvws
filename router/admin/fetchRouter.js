ba32fe70c8e6ebed9ed114d1825a432b754b35e8
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-21 17:48:30 +0530

    rectify pending form in bm 21.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -71,7 +71,6 @@ fetchRouter.get("/fetch_form_dtls_web", async (req, res) => {
     whr = `a.branch_code = b.branch_code 
     AND a.member_code = b.member_code 
     AND a.branch_code = '${data.branch_code}' 
-    AND b.form_no = '${data.form_no}' 
     AND b.approval_status = '${data.approval_status}'`,
     order = null;
     var fetch_basic = await db_Select(select,table_name,whr,order)
