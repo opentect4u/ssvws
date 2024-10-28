fcd8608caf6677f2894f8990ada19810718c2a4d
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-28 15:29:13 +0530

    add sign up 28.10.2024

--- router/admin/userRouter.js
+++ router/admin/userRouter.js
@@ -50,9 +50,14 @@ userRouter.post("/fetch_sign_up_dt", async (req, res) => {
             var sign_data = await db_Select(select,table_name,whr,null)
            
            if (sign_data.suc > 0){
-              res.send({ suc: 1, msg: "Already registered",  dtls: sign_data.msg[0] }); 
-           }else {
-             result = { suc: 2, msg: "No data found", dt: emp_dt };
+            if(sign_data.msg.length > 0){
+                res.send({ suc: 1, msg: "Already registered",  dtls: sign_data.msg[0] }); 
+            }else{
+                result = { suc: 1, msg: emp_dt.msg, dt: emp_dt };
+                res.send(result)
+            }
+           }else{
+             result = { suc: 2, msg: "error to fetch data", dt: emp_dt };
              res.send(result)
            }
     }else{
