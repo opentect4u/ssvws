8e63737b2f4235b4e830f91d4d537faeef8428a8
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-28 14:09:38 +0530

    add sign up 28.10.2024

--- router/admin/userRouter.js
+++ router/admin/userRouter.js
@@ -34,7 +34,7 @@ const { db_Select, db_Insert } = require('../../model/mysqlModel');
 // });
 
 userRouter.post("/fetch_sign_up_dt", async (req, res) => {
-    var data = req.body;
+    var data = req.body,result;
     console.log(data,'kkk');
     const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
@@ -44,20 +44,21 @@ userRouter.post("/fetch_sign_up_dt", async (req, res) => {
     var emp_dt = await db_Select(select,table_name,whr,null)
     
     if(emp_dt.suc > 0) {
-        if(emp_dt.msg.length > 0){
             var select = "emp_id,brn_code",
             table_name = "md_user",
             whr = `emp_id = '${data.emp_id}'`;
             var sign_data = await db_Select(select,table_name,whr,null)
-            res.send({ suc: 1, msg: "Already registered",  dtls: sign_data.msg[0] });     
-            }else {
-                result = { suc: 2, msg: "No data found", dt: emp_dt };
-                res.send(result)
-            }
-            }else {
-                result = { suc: 0, msg: emp_dt.msg, dt: emp_dt };
-                res.send(result)
-            }
+           
+           if (sign_data.suc > 0){
+              res.send({ suc: 1, msg: "Already registered",  dtls: sign_data.msg[0] }); 
+           }else {
+              res.send(sign_data)
+           }
+    }else{
+        result = { suc: 2, msg: "No data found", dt: emp_dt };
+        res.send(result)
+    }    
+           
 });
 
 module.exports = {userRouter}
