64a28b17bb3a1847beb1471fb71a6c9fa0757da2
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-29 11:49:23 +0530

    add branch name in outstanding section 29.10.2024

--- router/api/masterRouter.js
+++ router/api/masterRouter.js
@@ -204,12 +204,12 @@ masterRouter.post("/fetch_validation", async (req, res) => {
         var response_set = {}
         if(res_dt.suc > 0){
             if(res_dt.msg.length > 0){
-                var default_chk = await db_Select('outstanding', 'td_loan', `sub_customer_id=${res_dt.msg[0].member_code}`, `HAVING outstanding > 0`)
+                var default_chk = await db_Select('a.outstanding,b.branch_name', 'td_loan a LEFT JOIN md_branch b ON a.branch_id = b.branch_code', `sub_customer_id=${res_dt.msg[0].member_code}`, `HAVING outstanding > 0`)
                 if(default_chk.suc > 0){
                     if(default_chk.msg.length > 0){
-                        response_set = {suc: 0, msg: [], status: `You are not eligible to get a new Loan. You have Rs. ${default_chk.msg[0].outstanding} outstanding.`}
+                        response_set = {suc: 0, msg: [], status: `You are in ${default_chk.msg[0].branch_name} Branch and You are not eligible to get a new Loan. You have Rs. ${default_chk.msg[0].outstanding} outstanding.`}
                     }else{
-                        response_set = {suc: 1, msg: res_dt.msg, status: 'Existing User. Eligible to apply a new loan.'}
+                        response_set = {suc: 1, msg: res_dt.msg, status: `Existing User in ${default_chk.msg[0].branch_name} Branch and Eligible to apply a new loan.`}
                     }
                 }else{
                     response_set = {suc: 1, msg: res_dt.msg, status: 'Error to fetch outstanding data'}
