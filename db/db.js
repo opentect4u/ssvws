465d508330b1ae5ad814f473719dbcb5c313c504
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-29 12:42:03 +0530

    add branch name in outstanding section 29.10.2024

--- db/db.js
+++ db/db.js
@@ -2,21 +2,21 @@ const mysql = require("mysql");
 require('dotenv').config()
 
 // LOCAL //
-// const db = mysql.createPool({
-//   connectionLimit: 10,
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASS,
-//   database: process.env.MYSQL_DB,
-// });
-
 const db = mysql.createPool({
   connectionLimit: 10,
-  host: "202.21.38.178",
-  user: "root",
-  password: "root",
-  database: "ssvws",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
 });
+
+// const db = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "202.21.38.178",
+//   user: "root",
+//   password: "root",
+//   database: "ssvws",
+// });
 
 db.getConnection((err, connection) => {
   if (err) console.log(err);
