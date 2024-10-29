0b6fd25fd1ff94d28772d95f1a99e40afbd6f415
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-10-29 13:36:45 +0530

    add branch name in outstanding section 29.10.2024

--- router/admin/fetchRouter.js
+++ router/admin/fetchRouter.js
@@ -170,7 +170,7 @@ fetchRouter.post("/edit_basic_dtls_web", async (req, res) => {
 
     var select = "a.branch_code,a.member_code,a.client_name,b.form_no,b.grt_date,b.approval_status,b.remarks,b.rejected_by,b.rejected_at,c.branch_name",
     table_name = "md_member a LEFT JOIN td_grt_basic b ON a.branch_code = b.branch_code AND a.member_code = b.member_code LEFT JOIN md_branch c ON a.branch_code = c.branch_code",
-    whr = `a.branch_code IN (${data.branch_code}) AND b.approval_status = '${data.approval_status}' AND b.delete_flag = 'N'`,
+    whr = `a.branch_code = '${data.branch_code}' AND b.approval_status = '${data.approval_status}' AND b.delete_flag = 'N'`,
     order = null;
     var fetch_bm_fwd_dt = await db_Select(select,table_name,whr,order)
 
