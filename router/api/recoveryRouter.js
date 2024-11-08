08f4e7dae136628b845d5921f9b22bf8fc74ce50
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-08 13:16:59 +0530

    commit search_grp 08.11.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -14,11 +14,12 @@ recoveryRouter.post("/search_group_app", async (req, res) => {
     var search_grp = await db_Select (select,table_name,whr,order);
 
     if(search_grp.suc > 0 && search_grp.msg.length > 0){
-        var select = "a.loan_id,b.client_name",
+        var select = "a.loan_id,a.member_code,a.prn_emi,a.intt_emi,a.tot_emi,a.period,a.period_mode,a.instl_paid,a.instl_end_dt,b.client_name",
         table_name = "td_loan a LEFT JOIN md_member b ON a.branch_code = b.branch_code AND a.member_code = b.member_code",
         whr = `a.group_code = ${search_grp.msg[0].group_code}`,
         order = null;
         var mem_dt = await db_Select(select,table_name,whr,order);
+
         search_grp.msg[0]['memb_dtls'] = mem_dt.suc > 0 ? (mem_dt.msg.length > 0 ? mem_dt.msg : []) : [];
     }
 
