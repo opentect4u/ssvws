78381bfbc3628bc076cc9750c346e2d61ed8dbda
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-11 12:56:53 +0530

    commit recovery 11.11.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -10,14 +10,15 @@ recoveryRouter.post("/search_group_app", async (req, res) => {
 
     var select = "a.group_code,b.group_name,SUM(a.prn_amt + a.od_prn_amt) AS total_prn_amt,SUM(a.intt_amt + a.od_intt_amt) AS total_intt_amt,c.status,b.group_type",
     table_name = "td_loan a LEFT JOIN md_group b ON a.branch_code = b.branch_code AND a.group_code = b.group_code LEFT JOIN td_loan_transactions c ON a.branch_code = c.branch_id AND a.loan_id = c.loan_id",
-    whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%') AND c.status = 'A'`,
+    // whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%') AND c.status = 'A'`,
+    whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%')`,
     order = `GROUP BY a.group_code, b.group_name, b.group_type`;
     var search_grp = await db_Select (select,table_name,whr,order);
 
     if(search_grp.suc > 0 && search_grp.msg.length > 0){
         var select = "a.loan_id,a.member_code,a.prn_amt,a.od_prn_amt,a.intt_amt,a.od_intt_amt,a.prn_emi,a.intt_emi,a.tot_emi,a.period,a.period_mode,a.instl_paid,a.instl_end_dt,b.client_name,c.status",
         table_name = "td_loan a LEFT JOIN md_member b ON a.branch_code = b.branch_code AND a.member_code = b.member_code LEFT JOIN td_loan_transactions c ON a.branch_code = c.branch_id AND  a.loan_id = c.loan_id",
-        whr = `a.group_code = ${search_grp.msg[0].group_code} AND c.status = 'A'`,
+        whr = `a.group_code = ${search_grp.msg[0].group_code}`,
         order = null;
         var mem_dt = await db_Select(select,table_name,whr,order);
 
