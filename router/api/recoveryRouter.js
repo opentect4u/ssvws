1f7aadfd5c2b2a97465bb2ccbbb336c9dd442cea
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-11 13:10:14 +0530

    commit recovery 11.11.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -8,9 +8,9 @@ dateFormat = require('dateformat');
 recoveryRouter.post("/search_group_app", async (req, res) => {
     var data = req.body;
 
-    var select = "a.group_code,b.group_name,SUM(a.prn_amt + a.od_prn_amt) AS total_prn_amt,SUM(a.intt_amt + a.od_intt_amt) AS total_intt_amt,b.group_type",
+    var select = "a.group_code,b.group_name,SUM(a.prn_amt + a.od_prn_amt) AS total_prn_amt,SUM(a.intt_amt + a.od_intt_amt) AS total_intt_amt,c.status,b.group_type",
     table_name = "td_loan a LEFT JOIN md_group b ON a.branch_code = b.branch_code AND a.group_code = b.group_code LEFT JOIN td_loan_transactions c ON a.branch_code = c.branch_id AND a.loan_id = c.loan_id",
-    whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%')`,
+    whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%') AND c.status = 'A'`,
     order = `GROUP BY a.group_code, b.group_name, b.group_type`;
     var search_grp = await db_Select (select,table_name,whr,order);
 
