5c15785fc37d19f93266b29748c37dd98b2df504
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-14 10:22:41 +0530

    commit add min max week period 13.11.2024

--- modules/admin/loanModule.js
+++ modules/admin/loanModule.js
@@ -52,9 +52,8 @@ module.exports = {
             
             let instl_date = await genDate(data.period,data.period_mode,data.recovery_date,data.recovery_date);
 
-            const startDate = instl_date?.emiStartDate || 0;
-            const endDate = instl_date?.emiEndDate ? instl_date.emiEndDate.toISOString().slice(0, 10) : 0;
-
+            const startDate = instl_date.emiStartDate;
+            const endDate = instl_date.emiEndDate;
 
             console.log("Start Date:", startDate);
             console.log("End Date:", endDate);
