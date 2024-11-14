7a4727d09e92bd579d5c17b972e0ab27c3b8cb9f
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-14 18:31:38 +0530

    commit add min max week period 13.11.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -8,10 +8,10 @@ dateFormat = require('dateformat');
 recoveryRouter.post("/search_group_app", async (req, res) => {
     var data = req.body;
 
-    var select = "a.group_code,a.period,b.group_name,SUM(a.prn_amt + a.od_prn_amt) AS total_prn_amt,SUM(a.intt_amt + a.od_intt_amt) AS total_intt_amt,c.status,b.group_type",
+    var select = "a.group_code,a.curr_roi,b.group_name,SUM(a.prn_amt + a.od_prn_amt) AS total_prn_amt,SUM(a.intt_amt + a.od_intt_amt) AS total_intt_amt,c.status,b.group_type",
     table_name = "td_loan a LEFT JOIN md_group b ON a.branch_code = b.branch_code AND a.group_code = b.group_code LEFT JOIN td_loan_transactions c ON a.branch_code = c.branch_id AND a.loan_id = c.loan_id",
     whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%') AND c.status = 'A'`,
-    order = `GROUP BY a.group_code, a.period, b.group_name, b.group_type`;
+    order = `GROUP BY a.group_code, a.curr_roi, b.group_name, b.group_type`;
     var search_grp = await db_Select (select,table_name,whr,order);
 
     if(search_grp.suc > 0 && search_grp.msg.length > 0){
