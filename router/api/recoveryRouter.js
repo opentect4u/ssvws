9691d091664d738a059d230ff377d2c1a98e9401
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-15 17:57:42 +0530

    commit add recovery api 15.11.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -44,16 +44,22 @@ recoveryRouter.post("/recovery_transaction", async (req, res) => {
 
 });
 
-// recoveryRouter.post("/view_transaction", async (req, res) => {
-//     var data = req.body;
+recoveryRouter.post("/view_transaction", async (req, res) => {
+    var data = req.body;
 
-//     var select = "a.loan_id,a.branch_code,a.group_code,a.member_code,a.grt_form_no,a.purpose,a.sub_purpose,a.applied_amt,",
-//     table_name = "",
-//     whr = ``,
-//     order = "",
-//     var view_dt = await db_Select(select,table_name,whr,order);
+    var select = "a.loan_id,a.member_code,a.period,a.curr_roi,a.prn_disb_amt,a.intt_cal_amt,a.prn_amt,a.od_prn_amt,a.intt_amt,a.od_intt_amt,a.outstanding,a.prn_emi,a.intt_emi,a.tot_emi,a.recovery_day,a.period,a.period_mode,a.instl_paid,a.instl_start_dt,a.instl_end_dt,a.last_trn_dt,b.client_name,c.credit,c.debit,c.prn_recov,c.intt_recov,c.balance,c.intt_balance,c.recov_upto,c.tr_type,c.status,c.payment_date,c.trn_lat,c.trn_long,c.trn_addr",
+    table_name = "td_loan a, md_member b, td_loan_transactions c",
+    whr = `a.branch_code = b.branch_code 
+        AND a.member_code = b.member_code
+        AND a.branch_code = c.branch_id 
+        AND a.loan_id = c.loan_id
+        AND a.loan_id = '${data.loan_id}'
+        AND c.status = '${data.approval_status}'
+        AND c.tr_type = 'R'`,
+    order = "Order BY a.member_code";
+    var view_dt = await db_Select(select,table_name,whr,order);
 
-//     res.send(view_dt)
-// })
+    res.send(view_dt)
+})
 
 module.exports = {recoveryRouter}
