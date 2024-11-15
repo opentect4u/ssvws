44c5c45fc977ed2d5824fd23bae363283447290e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-15 13:51:43 +0530

    commit add recovery api 15.11.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -10,7 +10,7 @@ recoveryRouter.post("/search_group_app", async (req, res) => {
 
     var select = "a.group_code,b.group_name,SUM(a.prn_amt + a.od_prn_amt) AS total_prn_amt,SUM(a.intt_amt + a.od_intt_amt) AS total_intt_amt,c.status,b.group_type",
     table_name = "td_loan a LEFT JOIN md_group b ON a.branch_code = b.branch_code AND a.group_code = b.group_code LEFT JOIN td_loan_transactions c ON a.branch_code = c.branch_id AND a.loan_id = c.loan_id",
-    whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%') AND c.status = 'A' AND c.tr_type = 'D'`,
+    whr = `(a.group_code like '%${data.grp_dtls}%' OR b.group_name like '%${data.grp_dtls}%') AND c.status = 'A'`,
     order = `GROUP BY a.group_code, b.group_name, b.group_type`;
     var search_grp = await db_Select (select,table_name,whr,order);
 
@@ -21,7 +21,7 @@ recoveryRouter.post("/search_group_app", async (req, res) => {
         AND a.member_code = b.member_code
         AND a.branch_code = c.branch_id 
         AND  a.loan_id = c.loan_id
-        AND a.group_code = ${search_grp.msg[0].group_code} AND c.status = 'A' AND c.tr_type = 'D'
+        AND a.group_code = ${search_grp.msg[0].group_code} AND c.status = 'A'
         AND c.payment_date = (SELECT MAX(d.payment_date) FROM td_loan_transactions d WHERE a.branch_code = d.branch_id AND a.loan_id = d.loan_id)`,
         order = null;
         var mem_dt = await db_Select(select,table_name,whr,order);
