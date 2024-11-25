7806f3665339681fd42ee2489bc1b18c8e5b01b8
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-25 16:25:06 +0530

    add dashboard data 25.11.2024

--- router/api/dashboardRouter.js
+++ router/api/dashboardRouter.js
@@ -25,7 +25,7 @@ appdashboardRouter.post("/dashboard_dtls_cash_recov", async (req, res) => {
     whr = `branch_id = '${data.branch_code}'
     AND tr_type = 'R'
     AND tr_mode = '${data.tr_mode}'
-    AND date(grt_date) = '${data.datetime}'`,
+    AND date(payment_date) = '${data.datetime}'`,
     order = null;
     var dashboard_dt_cash = await db_Select(select,table_name,whr,order);
 
@@ -40,7 +40,7 @@ appdashboardRouter.post("/dashboard_dtls_bank_recov", async (req, res) => {
     whr = `branch_id = '${data.branch_code}'
     AND tr_type = 'R'
     AND tr_mode = '${data.tr_mode}'
-    AND date(grt_date) = '${data.datetime}'`,
+    AND date(payment_date) = '${data.datetime}'`,
     order = null;
     var dashboard_dt_bank = await db_Select(select,table_name,whr,order);
 
