1460becc229b4ceacd9cdd02fc888d1c9c031df8
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-25 15:49:39 +0530

    add dashboard data 25.11.2024

--- router/api/dashboardRouter.js
+++ router/api/dashboardRouter.js
@@ -14,6 +14,19 @@ appdashboardRouter.post("/dashboard_dtls", async (req, res) => {
     order = null;
     var dashboard_dt = await db_Select(select,table_name,whr,order);
 
+    if (dashboard_dt.suc > 0 && dashboard_dt.msg.length > 0){
+        var select = "SUM(credit)",
+        table_name = "td_loan_transactions",
+        whr = `branch_id = '${data.branch_code}'
+        AND  tr_type = 'R'
+        AND tr_mode = '${data.tr_mode}'
+        AND payment_date = '${data.datetime}'`,
+        order = null;
+    var dashboard_dtls = await db_Select(select,table_name,whr,order);
+    }
+
+    dashboard_dt.msg[0]['app_dashboard_dt'] = dashboard_dtls.suc > 0 ? (dashboard_dtls.msg.length > 0 ? dashboard_dtls.msg : []) : [];
+
     res.send(dashboard_dt)
 })
 
