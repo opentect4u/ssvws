290d28a60941eec2e7a6eb3de683f81997e20b3e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-11-26 18:10:23 +0530

    add dashboard data for bm 26.11.2024

--- router/api/dashboardRouter.js
+++ router/api/dashboardRouter.js
@@ -17,22 +17,6 @@ appdashboardRouter.post("/dashboard_dtls", async (req, res) => {
     res.send(dashboard_dt)
 });
 
-appdashboardRouter.post("/dashboard_dtls_bm", async (req, res) => {
-    var data = req.body;
-    console.log(data);
-    
-        var select = "COUNT(form_no) no_of_grt",
-        table_name = "td_grt_basic",
-        whr = data.flag == 'O' ? 
-        `modified_by = ${data.emp_id} AND date(grt_date) = '${data.datetime}'` 
-        : `branch_code = '${data.branch_code}' AND date(grt_date) = '${data.datetime}'`,
-        order = null;
-        var dashboard_dt = await db_Select(select,table_name,whr,order);
-
-    res.send(dashboard_dt)
-});
-
-
 appdashboardRouter.post("/dashboard_dtls_cash_recov", async (req, res) => {
     var data = req.body;
 
@@ -63,6 +47,61 @@ appdashboardRouter.post("/dashboard_dtls_bank_recov", async (req, res) => {
     var dashboard_dt_bank = await db_Select(select,table_name,whr,order);
 
     res.send(dashboard_dt_bank)
+});
+
+appdashboardRouter.post("/dashboard_dtls_bm", async (req, res) => {
+    var data = req.body;
+    console.log(data);
+    
+        var select = "COUNT(form_no) no_of_grt",
+        table_name = "td_grt_basic",
+        whr = data.flag == 'O' ? 
+        `modified_by = ${data.emp_id} AND date(grt_date) = '${data.datetime}'` 
+        : `branch_code = '${data.branch_code}' AND date(grt_date) = '${data.datetime}'`,
+        order = null;
+        var dashboard_dt = await db_Select(select,table_name,whr,order);
+
+    res.send(dashboard_dt)
+});
+
+appdashboardRouter.post("/dashboard_dtls_cash_recov_bm", async (req, res) => {
+    var data = req.body;
+
+    var select = "SUM(credit) tot_recov_cash",
+    table_name = "td_loan_transactions",
+    whr = data.flag == 'O' ? `branch_id = '${data.branch_code}'
+    AND tr_type = 'R'
+    AND tr_mode = '${data.tr_mode}'
+    AND date(payment_date) = '${data.datetime}'
+    AND created_by = ${data.emp_id}` 
+    : `branch_id = '${data.branch_code}'
+     AND tr_type = 'R'
+    AND tr_mode = '${data.tr_mode}'
+    AND date(payment_date) = '${data.datetime}'`,
+    order = null;
+    var dashboard_dt_cash_bm = await db_Select(select,table_name,whr,order);
+
+    res.send(dashboard_dt_cash_bm)
+});
+
+appdashboardRouter.post("/dashboard_dtls_bank_recov_bm", async (req, res) => {
+    var data = req.body;
+
+    var select = "SUM(credit) tot_recov_bank",
+    table_name = "td_loan_transactions",
+    whr =  data.flag == 'O' ? `branch_id = '${data.branch_code}'
+    AND tr_type = 'R'
+    AND tr_mode = '${data.tr_mode}'
+    AND date(payment_date) = '${data.datetime}'
+    AND created_by = ${data.emp_id}` 
+    : `branch_id = '${data.branch_code}'
+    AND tr_type = 'R'
+    AND tr_mode = '${data.tr_mode}'
+     AND date(payment_date) = '${data.datetime}'`,
+    order = null;
+    var dashboard_dt_bank_bm = await db_Select(select,table_name,whr,order);
+
+    res.send(dashboard_dt_bank_bm)
 });
 
 module.exports = {appdashboardRouter}
