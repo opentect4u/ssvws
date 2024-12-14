f3bbed1f33133e23275e42330e2c7fc90056366b
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-12-14 18:55:33 +0530

    commit 14.12.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -148,6 +148,8 @@ recoveryRouter.post("/search_group_app", async (req, res) => {
 
 recoveryRouter.post("/recovery_transaction", async (req, res) => {
     var data = req.body,res_dt;
+    console.log(data,'dt');
+    
     recovery_trans(data).then(data => {
         res_dt = data
     }).catch(err => {
