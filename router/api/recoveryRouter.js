0ade427b04f031f42398feefefd9e07e6616c60b
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-12-14 12:48:24 +0530

    commit 14.12.2024

--- router/api/recoveryRouter.js
+++ router/api/recoveryRouter.js
@@ -121,8 +121,7 @@ recoveryRouter.post("/search_group_app", async (req, res) => {
                 whr = `loan_id = '${memb.loan_id}'
                 AND payment_date = (SELECT MAX(payment_date) 
                                                FROM td_loan_transactions 
-                                               WHERE loan_id = '${memb.loan_id}'
-                                               AND tr_type != 'I')`;
+                                               WHERE loan_id = '${memb.loan_id}')`;
                 var order = null;
             
                 var balance_dt = await db_Select(select, table_name, whr, order);
