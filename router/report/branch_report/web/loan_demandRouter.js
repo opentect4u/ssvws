803cccd2148188e61d6d918701e4231660fad5c5
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-16 16:02:55 +0530

    commit loan_summary_report 16.01.2025

--- router/report/branch_report/web/loan_demandRouter.js
+++ router/report/branch_report/web/loan_demandRouter.js
@@ -55,36 +55,35 @@ loan_demandRouter.post("/loan_demand_report", async (req, res) => {
                 var instl_end_dt = dt.instl_end_dt;
 
                 // Calculate balance
-                var select = "(balance + od_balance + intt_balance) balance";
-                table_name = "td_loan_transactions";
-                whr = `loan_id = '${loan_id}' AND tr_type = 'R'
-                                    AND payment_date = (SELECT MAX(payment_date)
-                                                    FROM td_loan_transactions
-                                                    WHERE loan_id = '${loan_id}' 
-                                                    AND payment_date <= '${data.to_dt}'
-                                                    AND tr_type = 'R')`;
-                var details = await db_Select(select, table_name, whr, null);
+                // var select = "(balance + od_balance + intt_balance) balance";
+                // table_name = "td_loan_transactions";
+                // whr = `loan_id = '${loan_id}' AND tr_type = 'R'
+                //                     AND payment_date = (SELECT MAX(payment_date)
+                //                                     FROM td_loan_transactions
+                //                                     WHERE loan_id = '${loan_id}' 
+                //                                     AND payment_date <= '${data.to_dt}'
+                //                                     AND tr_type = 'R')`;
+                // var details = await db_Select(select, table_name, whr, null);
                 // console.log(details, 'details');
                  
                 // Check balance
-                if (details.suc > 0 && details.msg.length > 0) {
-                        if (details.msg[0].balance > 0) {
+                // if (details.suc > 0 && details.msg.length > 0) {
+                        // if (details.msg[0].balance > 0) {
 
                              var balance_dt = details.msg[0].balance
                                 // console.log(balance_dt,'balance');
                 
                              var demandData = await getLoanDmd(loan_id, data.to_dt);
 
-                            if(demandData.suc > 0 && demandData.demand.ld_demand > 0){
+                            // if(demandData.suc > 0 && demandData.demand.ld_demand > 0){
                                 demandResults.push({ loan_id,branch_code,group_code,group_name,member_code,client_name,disb_dt,curr_roi,period,period_mode,balance_dt,co_name,tot_emi,instl_end_dt, demand: demandData.demand.ld_demand });
-                            }else {
-                            // console.log({ suc: 0, msg: 'Demand Amount is 0' });
-                            }
-                        }
+                            // }else {
+                            // }
+                        // }
                 
-                } else {
-                    // console.log(`Loan ID ${loan_id}: Balance is 0.`);
-                }
+                // } else {
+                //     // console.log(`Loan ID ${loan_id}: Balance is 0.`);
+                // }
             }
 
             res.send({ suc: 1, msg: demandResults });
