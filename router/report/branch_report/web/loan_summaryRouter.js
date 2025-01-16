afe1899243c435e8857bba2aa03360b41fd42960
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-16 11:20:33 +0530

    commit loan_summary_report 16.01.2025

--- router/report/branch_report/web/loan_summaryRouter.js
+++ router/report/branch_report/web/loan_summaryRouter.js
@@ -23,13 +23,14 @@ loan_summaryrouter.post ("/loan_summary_report_fundwise", async (req, res) => {
 loan_summaryrouter.post ("/loan_summary_report_schemewise", async (req, res) => {
     var data = req.body;
 
-    var select = "a.scheme_id,c.fund_name,sum(b.debit) tot_debit,sum(b.credit) tot_credit,sum(a.outstanding) tot_outstanding",
+    var select = "a.scheme_id,c.scheme_name,sum(b.debit) tot_debit,sum(b.credit) tot_credit,sum(a.outstanding) tot_outstanding",
     table_name = "td_loan a,td_loan_transactions b, md_scheme c",
     whr = `a.loan_id = b.loan_id 
+           AND a.scheme_id = c.scheme_id
            AND b.tr_type IN ('D','R')
            AND a.branch_code = '${data.branch_code}'
            AND date(b.payment_date) BETWEEN '${data.from_dt}' AND '${data.to_dt}'`
-    order = `GROUP BY a.scheme_id`;
+    order = `GROUP BY a.scheme_id,c.scheme_name`;
     var fund_dt = await db_Select(select,table_name,whr,order);
 
     res.send(fund_dt)
