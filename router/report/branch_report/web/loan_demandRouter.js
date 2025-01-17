87ace7e6c41745838dec65eb72cf55c33a1e8b2b
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-17 16:17:42 +0530

    commit demand report 17.01.2025

--- router/report/branch_report/web/loan_demandRouter.js
+++ router/report/branch_report/web/loan_demandRouter.js
@@ -104,53 +104,89 @@ dateFormat = require('dateformat');
 //     }
 // });
 
-loan_demandRouter.post("/loan_demand_report", async (req, res) => {
+
+//MEMBERWISE DEMAND REPORT
+loan_demandRouter.post("/loan_demand_report_membwise", async (req, res) => {
     try {
                 var data = req.body;
 
-                // Extract day from from_dt
-                var fromdt = dateFormat(data.from_dt, "dd");
-
-                // Extract day from to_dt
-                var todt = dateFormat(data.to_dt, "dd")
-
-                // Extract day of the week from the current date
-
-                var selDayNum = dayRevarseList[dateFormat(data.to_dt, 'dddd')];
-
                  // Fetch loan details from recovery day
-
                  const pro_name = 'p_loan_demand',
                  pro_params = `?,?,?`,
-                 pro_params_val = [fromdt, todt, data.branch_code],
-                 sel_table_name = `td_loan a
-                                   LEFT JOIN md_group b ON a.group_code = b.group_code
-                                   LEFT JOIN md_member c ON a.member_code = c.member_code
-                                   LEFT JOIN md_employee d ON b.created_by = d.emp_id`,
-                 sel_fields = `a.loan_id,a.branch_code,a.group_code,b.group_name,a.member_code,c.client_name,a.disb_dt,a.curr_roi,a.period,a.period_mode,a.recovery_day,a.instl_end_dt,a.tot_emi,b.created_by collec_code,d.emp_name co_name`,
-                 sel_whr_fields = null,
+                 pro_params_val = [data.from_dt, data.to_dt, data.branch_code],
+                 sel_table_name = `tt_loan_demand a,vw_loan_demand_report b`,
+                 sel_fields = `a.loan_id,a.member_code,b.client_name,a.group_code,b.group_name,a.disbursed_date,a.disbursed_amount,a.current_roi,a.period,a.period_mode,a.recov_day,a.installment_end_date,a.total_emi,a.curr_dmd_amt demand,a.current_principal,b.co_name`,
+                 sel_whr_fields = `a.loan_id = b.loan_id AND (a.curr_dmd_amt + a.current_principal) > 0`,
                  sel_whr_arr = [],
-                 sel_order = `ORDER BY a.loan_id,a.group_code`;
-                 var loan_dt = await db_RunProcedureAndFetchData(pro_name, pro_params, pro_params_val, sel_fields, sel_table_name, sel_whr_fields, sel_whr_arr, sel_order);
+                 sel_order = `ORDER BY  a.loan_id`;
+                 var loan_dt_membwise = await db_RunProcedureAndFetchData(pro_name, pro_params, pro_params_val, sel_fields, sel_table_name, sel_whr_fields, sel_whr_arr, sel_order);
 
-               if (loan_dt.suc > 0 && loan_dt.msg.length > 0) {
-                 var demandResults = [];
+                 res.send(loan_dt_membwise)
 
-                  for (let dt of loan_dt.msg) {
-                    var get_balance = await getLoanBal(dt.loan_id, data.to_dt);
-                    var demandData = await getLoanDmd(dt.loan_id, data.to_dt);
-                    dt['demand'] = demandData.demand.ld_demand
-                    dt['balance_dt'] = get_balance.balance
-                  }
-                 res.send(loan_dt)
-                } else {
-                 res.send({ suc: 0, msg: [] });
-                }
+              //  if (loan_dt.suc > 0 && loan_dt.msg.length > 0) {
+              //    var demandResults = [];
+
+              //     for (let dt of loan_dt.msg) {
+              //       var get_balance = await getLoanBal(dt.loan_id, data.to_dt);
+              //       var demandData = await getLoanDmd(dt.loan_id, data.to_dt);
+              //       dt['demand'] = demandData.demand.ld_demand
+              //       dt['balance_dt'] = get_balance.balance
+              //     }
+              //    res.send(loan_dt)
+              //   } else {
+              //    res.send({ suc: 0, msg: [] });
+              //   }
+
+            } catch (error) {
+              console.error("Error fetching demand report memberwise:", error);
+              res.send({ suc: 0, msg: "An error occurred" });
+          }
+});
+
+
+//GROUPWISE DEMAND REPORT
+loan_demandRouter.post("/loan_demand_report_groupwise", async (req, res) => {
+  try {
+              var data = req.body;
+
+               const pro_name = 'p_loan_demand',
+               pro_params = `?,?,?`,
+               pro_params_val = [data.from_dt, data.to_dt, data.branch_code],
+               sel_table_name = `tt_loan_demand a,vw_loan_demand_report b`,
+               sel_fields = `a.group_code,b.group_name,a.disbursed_date,sum(a.disbursed_amount)disbursed_amount,a.current_roi,a.period,a.period_mode,a.installment_end_date,sum(a.total_emi)total_emi,sum(a.curr_dmd_amt) demand,sum(a.current_principal)outstanding,b.co_name`,
+               sel_whr_fields = `a.loan_id = b.loan_id AND (a.curr_dmd_amt + a.current_principal) > 0`,
+               sel_whr_arr = [],
+               sel_order = `GROUP BY a.group_code,b.group_name,a.disbursed_date,a.current_roi,a.period,a.period_mode,a.installment_end_date,b.co_name`;
+               var loan_dt_groupwise = await db_RunProcedureAndFetchData(pro_name, pro_params, pro_params_val, sel_fields, sel_table_name, sel_whr_fields, sel_whr_arr, sel_order);
+
+               res.send(loan_dt_groupwise)
+
+          } catch (error) {
+            console.error("Error fetching demand report groupwise:", error);
+            res.send({ suc: 0, msg: "An error occurred" });
+        }
+});
+
+//COWISE DEMAND REPORT
+loan_demandRouter.post("/loan_demand_report_cowise", async (req, res) => {
+    try {
+        var data = req.body;
+        const pro_name = 'p_loan_demand',
+        pro_params = `?,?,?`,
+        pro_params_val = [data.from_dt, data.to_dt, data.branch_code],
+        sel_table_name = `tt_loan_demand a,vw_loan_demand_report b`,
+        sel_fields = `a.loan_id,a.member_code,b.client_name,a.group_code,b.group_name,a.disbursed_date,a.disbursed_amount,a.current_roi,a.period,a.period_mode,a.recov_day,a.installment_end_date,a.total_emi,a.curr_dmd_amt demand,a.current_principal,b.co_name,b.collec_code`,
+        sel_whr_fields = `a.loan_id = b.loan_id AND b.collec_code = '${data.co_id}' AND (a.curr_dmd_amt + a.current_principal) > 0`,
+        sel_whr_arr = [],
+        sel_order = `ORDER BY  a.loan_id`;
+        var repo_data_cowise = await db_RunProcedureAndFetchData(pro_name, pro_params, pro_params_val, sel_fields, sel_table_name, sel_whr_fields, sel_whr_arr, sel_order);
+
+        res.send(repo_data_cowise)
 
     } catch (error) {
-                console.error(error);
-                res.status(500).send({ suc: 0, msg: 'Internal Server Error' });
-            }
+        console.error("Error fetching demand vs collection report cowise:", error);
+        res.send({ suc: 0, msg: "An error occurred" });
+    }
 });
 
 module.exports = {loan_demandRouter}
