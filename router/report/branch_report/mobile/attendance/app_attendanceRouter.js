5ff84a100c54d2d6d9e4c93cce01325b5d5d65fb
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-24 12:02:45 +0530

    add attendancerouter 24.01.2025

--- router/report/branch_report/mobile/attendance/app_attendanceRouter.js
+++ router/report/branch_report/mobile/attendance/app_attendanceRouter.js
@@ -10,7 +10,8 @@ app_attendanceRouter.post("/attendance_report", async (req, res) => {
         var data = req.body;
         
         var get_last_day = await fetch_last_date(data.get_year,data.get_month)
-    
+        console.log(get_last_day,'last');
+        
         var select = "a.sl_no,a.in_date_time,a.out_date_time,b.emp_name",
         table_name = "td_emp_attendance a, md_employee b",
         whr = `a.emp_id = b.emp_id AND date(a.in_date_time) BETWEEN '${get_last_day}' AND '${get_last_day}' AND b.emp_id = '${data.emp_id}'`,
