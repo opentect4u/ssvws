d84ef750844cd364e141495dfa2d10506b1d9f1e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-24 12:43:49 +0530

    add attendancerouter 24.01.2025

--- router/report/branch_report/mobile/attendance/app_attendanceRouter.js
+++ router/report/branch_report/mobile/attendance/app_attendanceRouter.js
@@ -22,6 +22,16 @@ app_attendanceRouter.post("/attendance_report", async (req, res) => {
         order = null;
         var atten_report_dt = await db_Select(select,table_name,whr,order);
 
+        if(atten_report_dt.suc > 0 && atten_report_dt.msg.length > 0){
+            var select = "in_date_time,in_addr,out_date_time,out_addr,attan_status,attan_status == 'R' ? 'attan_reject_remarks' : '',late_in",
+            table_name = "td_emp_attendance",
+            whr = `emp_id = '${data.emp_id}'`,
+            order = null;
+            var atten_report_dtls = await db_Select(select,table_name,whr,order);
+            atten_report_dt.msg[0]['attens_dtls'] = atten_report_dtls.suc > 0 ? (atten_report_dtls.msg.length > 0 ? atten_report_dtls.msg : []) : []; 
+
+        }
+
         res.send(atten_report_dt)
     } catch (error) {
         console.error("Error fetching attendance report:", error);
