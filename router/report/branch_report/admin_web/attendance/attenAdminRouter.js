941bffa2cf0f48062fa9b751373b6d6335e8f9eb
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-24 18:29:37 +0530

    add attendancerouter 24.01.2025

--- router/report/branch_report/admin_web/attendance/attenAdminRouter.js
+++ router/report/branch_report/admin_web/attendance/attenAdminRouter.js
@@ -11,7 +11,7 @@ attenAdminRouter.post("/attendance_report_admin", async (req, res) => {
     if(data.branch_id == 'A'){
              var select = "a.emp_id,a.in_date_time,a.out_date_time,a.in_addr,a.out_addr,a.attan_status,a.clock_status,a.attn_reject_remarks,a.late_in,a.early_out,b.emp_name",
              table_name = "td_emp_attendance a, md_employee b",
-             whr = `a.emp_id = b.emp_id AND a.date(in_date_time) BETWEEN '${data.from_date}' AND '${data.to_date}'`
+             whr = `a.emp_id = b.emp_id AND date(a.in_date_time) BETWEEN '${data.from_date}' AND '${data.to_date}'`
              order = null;
              var atten_report = await db_Select(select,table_name,whr,order);
 
@@ -19,7 +19,7 @@ attenAdminRouter.post("/attendance_report_admin", async (req, res) => {
            }else {
             var select = "a.emp_id,a.in_date_time,a.out_date_time,a.in_addr,a.out_addr,a.attan_status,a.clock_status,a.attn_reject_remarks,a.late_in,a.early_out,b.emp_name",
             table_name = "td_emp_attendance a, md_employee b",
-            whr = `a.emp_id = b.emp_id AND a.date(in_date_time) BETWEEN '${data.from_date}' AND '${data.to_date}' AND b.branch_id = '${data.branch_id}'`,
+            whr = `a.emp_id = b.emp_id AND date(a.in_date_time) BETWEEN '${data.from_date}' AND '${data.to_date}' AND b.branch_id = '${data.branch_id}'`,
             order = null;
             var atten_report = await db_Select(select,table_name,whr,order);
 
