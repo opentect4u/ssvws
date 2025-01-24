b56611168679161036d77e802550a1a961001a61
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-24 18:28:17 +0530

    add attendancerouter 24.01.2025

--- router/report/branch_report/admin_web/attendance/attenAdminRouter.js
+++ router/report/branch_report/admin_web/attendance/attenAdminRouter.js
@@ -15,7 +15,7 @@ attenAdminRouter.post("/attendance_report_admin", async (req, res) => {
              order = null;
              var atten_report = await db_Select(select,table_name,whr,order);
 
-             res.send(scheme_dt)
+             res.send(atten_report)
            }else {
             var select = "a.emp_id,a.in_date_time,a.out_date_time,a.in_addr,a.out_addr,a.attan_status,a.clock_status,a.attn_reject_remarks,a.late_in,a.early_out,b.emp_name",
             table_name = "td_emp_attendance a, md_employee b",
