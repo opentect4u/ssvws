d04a1244f0ac821f70a6e978bf0933efe08ec9a6
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-24 18:24:56 +0530

    add attendancerouter 24.01.2025

--- router/report/branch_report/admin_web/attendance/attenAdminRouter.js
+++ router/report/branch_report/admin_web/attendance/attenAdminRouter.js
@@ -5,15 +5,30 @@ attenAdminRouter = express.Router(),
 dateFormat = require('dateformat');
 
 attenAdminRouter.post("/attendance_report_admin", async (req, res) => {
+    try{
     var data = req.body;
 
-    var select = "a.emp_id,a.in_date_time,a.out_date_time,a.in_addr,a.out_addr,a.attan_status,a.clock_status,a.attn_reject_remarks,a.late_in,a.early_out,b.emp_name",
-    table_name = "td_emp_attendance a, md_employee b",
-    whr = `a.emp_id = b.emp_id AND a.date(in_date_time) BETWEEN '${data.from_date}' AND '${data.to_date}' AND b.branch_id = '${data.branch_id}'`,
-    order = null;
-    var atten_report = await db_Select(select,table_name,whr,order);
+    if(data.branch_id == 'A'){
+             var select = "a.emp_id,a.in_date_time,a.out_date_time,a.in_addr,a.out_addr,a.attan_status,a.clock_status,a.attn_reject_remarks,a.late_in,a.early_out,b.emp_name",
+             table_name = "td_emp_attendance a, md_employee b",
+             whr = `a.emp_id = b.emp_id AND a.date(in_date_time) BETWEEN '${data.from_date}' AND '${data.to_date}'`
+             order = null;
+             var atten_report = await db_Select(select,table_name,whr,order);
 
-    res.send(atten_report)
+             res.send(scheme_dt)
+           }else {
+            var select = "a.emp_id,a.in_date_time,a.out_date_time,a.in_addr,a.out_addr,a.attan_status,a.clock_status,a.attn_reject_remarks,a.late_in,a.early_out,b.emp_name",
+            table_name = "td_emp_attendance a, md_employee b",
+            whr = `a.emp_id = b.emp_id AND a.date(in_date_time) BETWEEN '${data.from_date}' AND '${data.to_date}' AND b.branch_id = '${data.branch_id}'`,
+            order = null;
+            var atten_report = await db_Select(select,table_name,whr,order);
+
+            res.send(atten_report)
+         }
+        } catch (error) {
+            console.error("Error fetching attendance report:", error);
+            res.send({ suc: 0, msg: "An error occurred" });
+        }
 });
 
 module.exports = {attenAdminRouter}
