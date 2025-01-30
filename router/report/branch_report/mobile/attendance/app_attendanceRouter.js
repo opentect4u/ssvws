2f8b4d69a73d29e9c074f551ddd17e68c7ecd5cf
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-01-30 10:12:29 +0530

    add attendancerouter 30.01.2025

--- router/report/branch_report/mobile/attendance/app_attendanceRouter.js
+++ router/report/branch_report/mobile/attendance/app_attendanceRouter.js
@@ -9,16 +9,17 @@ app_attendanceRouter.post("/attendance_report", async (req, res) => {
     try {
         var data = req.body;
         
-        var get_last_day = await fetch_last_date(data.get_year,data.get_month)
+        // var get_last_day = await fetch_last_date(data.get_year,data.get_month)
         // console.log(get_last_day,'last');
         
-        var get_first_day = (data.get_year) + '-' + (data.get_month) + '-' + '01'
+        // var get_first_day = (data.get_year) + '-' + (data.get_month) + '-' + '01'
         // console.log(get_first_day,'ju');
         
         
         var select = "a.sl_no,a.entry_dt,a.in_date_time,a.out_date_time,a.clock_status,a.late_in,b.emp_name",
         table_name = "td_emp_attendance a, md_employee b",
-        whr = `a.emp_id = b.emp_id AND date(a.in_date_time) BETWEEN '${get_first_day}' AND '${get_last_day}' AND b.emp_id = '${data.emp_id}'`,
+        // whr = `a.emp_id = b.emp_id AND date(a.in_date_time) BETWEEN '${get_first_day}' AND '${get_last_day}' AND b.emp_id = '${data.emp_id}'`,
+        whr = `a.emp_id = b.emp_id AND a.entry_dt = '${data.entry_dt}' AND b.emp_id = '${data.emp_id}'`,
         order = null;
         var atten_report_dt = await db_Select(select,table_name,whr,order);
       
