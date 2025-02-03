7380ffaa7e0f22bd21c4d22981a964a69c6e8870
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-03 11:22:12 +0530

    add designation 03.02.2025

--- router/admin/designation/desigRouter.js
+++ router/admin/designation/desigRouter.js
@@ -28,5 +28,28 @@ desigRouter.get("/show_all_designation", async (req, res) => {
     var desig_dt = await db_Select(select, table_name,whr,order);
     
     res.send(desig_dt)
+});
+
+desigRouter.post("/fetch_designation", async (req, res) => {
+    try {
+        var data = req.body;
+    
+        //fetch all designation details
+        var select = "desig_type",
+        table_name = "md_designation",
+        whr = `desig_code = '${data.desig_code}'`,
+        order = null;
+        var fetch_desig_dtls = await db_Select(select,table_name,whr,order);
+    
+        // res.send(fetch_emp_dtls)
+        if (fetch_desig_dtls) {
+            res.send(fetch_desig_dtls);
+        } else {
+            res.send({ message: "Designation details not found." });
+        }
+    } catch (error) {
+        console.error("Error fetching designation details:", error);
+        res.send({ message: "An error occurred while fetching designation details.", error: error.message });
+    }
 })
 module.exports = {desigRouter}
