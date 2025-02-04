d268e5c457b4c3561f02b30d9bfbcc8acdfc1d64
Tan <mondal.tanmoy@synergicsoftek.com>
2025-02-04 16:16:05 +0530

    commit 04.02.2025

--- router/api/userMenuRouter.js
+++ router/api/userMenuRouter.js
@@ -4,7 +4,7 @@ const userMenuRouter = require('express').Router()
 
 userMenuRouter.post('/get_menu', async (req, res) => {
     var user_type_id = req.body.user_type_id
-    var select = 'a.user_type_id, a.menu_id, b.sl_no, b.key_id as "key", b.icon_name icon, b.link, b.label_name label, b.has_child',
+    var select = 'a.user_type_id, a.menu_id, b.menu_type, b.sl_no, b.key_id as "key", b.icon_name icon, b.link, b.label_name label, b.has_child, b.parent_id',
     table_name = 'td_user_menu a, md_menu b',
     whr = `a.menu_id=b.id AND b.menu_type = 'P' AND a.user_type_id = ${user_type_id} AND a.active_flag = 'Y' AND b.active_flag = 'Y'`,
     order = 'ORDER BY b.id';
@@ -13,7 +13,7 @@ userMenuRouter.post('/get_menu', async (req, res) => {
     if(parent_menu_list.suc > 0 && parent_menu_list.msg.length > 0){
         for(let dt of parent_menu_list.msg){
             if(dt.has_child != 'N'){
-                var select = 'a.user_type_id, b.sl_no, b.key_id as "key", b.icon_name icon, b.link, b.label_name label, b.has_child',
+                var select = 'a.user_type_id, a.menu_id, b.menu_type, b.sl_no, b.key_id as "key", b.icon_name icon, b.link, b.label_name label, b.has_child, b.parent_id',
                 table_name = 'td_user_menu a, md_menu b',
                 whr = `a.menu_id=b.id AND b.menu_type = 'C' AND a.user_type_id = ${user_type_id} AND b.parent_id = ${dt.menu_id} AND a.active_flag = 'Y' AND b.active_flag = 'Y'`,
                 order = 'ORDER BY b.sl_no';
@@ -21,7 +21,7 @@ userMenuRouter.post('/get_menu', async (req, res) => {
                 if(child_menu_list.suc > 0 && child_menu_list.msg.length > 0){
                     for(let cdt of child_menu_list.msg){
                         if(cdt.has_child != 'N'){
-                            var select = 'a.user_type_id, b.sl_no, b.key_id as "key", b.icon_name icon, b.link, b.label_name label, b.has_child',
+                            var select = 'a.user_type_id, a.menu_id, b.menu_type, b.sl_no, b.key_id as "key", b.icon_name icon, b.link, b.label_name label, b.has_child, b.parent_id',
                             table_name = 'td_user_menu a, md_menu b',
                             whr = `a.menu_id=b.id AND b.menu_type = 'C' AND a.user_type_id = ${user_type_id} AND b.parent_id = ${dt.menu_id} AND a.active_flag = 'Y' AND b.active_flag = 'Y'`,
                             order = 'ORDER BY b.sl_no';
