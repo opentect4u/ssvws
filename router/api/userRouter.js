e141cb48100f42ad2d635b90c47588e89c037289
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-04 11:27:25 +0530

    add branch in superadmin 04.02.2025

--- router/api/userRouter.js
+++ router/api/userRouter.js
@@ -1,17 +1,29 @@
 const userRouter = require('express').Router();
 dateFormat = require('dateformat');
 const bcrypt = require("bcrypt");
-const { app_login_data, bm_login_data } = require('../../modules/api/userModule');
-const { db_Insert } = require('../../model/mysqlModel');
+const { app_login_data, bm_login_data, superadmin_login_data } = require('../../modules/api/userModule');
+const { db_Insert, db_Select } = require('../../model/mysqlModel');
 
 userRouter.post('/login_app', async (req, res) => {
     var data = req.body,
         result;
     const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
-    //login app
-    var log_dt = await app_login_data(data);
-    // console.log(log_dt);
+    var select = "type_code,user_type",
+    table_name = "md_user_type",
+    whr = null,
+    order = null;
+    var user_type_dt = await db_Select(select,table_name,whr,order);
+
+    if(user_type_dt.suc > 0 && user_type_dt.msg.length > 0){
+       if(user_type_dt.msg[0].type_code == '1' && user_type_dt.msg[0].type_code == '2' && user_type_dt.msg[0].type_code == '5'){
+          //login app
+          var log_dt = await app_login_data(data);
+          // console.log(log_dt);
+       }else {
+          var log_dt = await superadmin_login_data(data);
+       }
+    }
    
     if (log_dt.suc > 0) {
         if (log_dt.msg.length > 0) {
