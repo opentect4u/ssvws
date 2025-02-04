7fa551a662dd8975d2a31c2f24abf4ca74116024
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-04 12:02:53 +0530

    add branch in superadmin 04.02.2025

--- router/api/userRouter.js
+++ router/api/userRouter.js
@@ -4,60 +4,63 @@ const bcrypt = require("bcrypt");
 const { app_login_data, bm_login_data, superadmin_login_data } = require('../../modules/api/userModule');
 const { db_Insert, db_Select } = require('../../model/mysqlModel');
 
-// userRouter.post('/login_app', async (req, res) => {
-//   try{
-//     var data = req.body,
-//         result;
-//     const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+userRouter.post('/logins_app', async (req, res) => {
+  try{
+    var data = req.body,
+        result;
+    const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
-//     var select = "type_code,user_type",
-//     table_name = "md_user_type",
-//     whr = null,
-//     order = null;
-//     var user_type_dt = await db_Select(select,table_name,whr,order);
+    var select = "type_code,user_type",
+    table_name = "md_user_type",
+    whr = null,
+    order = null;
+    var user_type_dt = await db_Select(select,table_name,whr,order);
 
-//     if (user_type_dt.suc > 0 || user_type_dt.msg.length > 0) {
-//       return res.send({ suc: 0, msg: "Invalid User Type" });
-//     }
+    if (user_type_dt.suc > 0 || user_type_dt.msg.length > 0) {
+      return res.send({ suc: 0, msg: "Invalid User Type" });
+    }
 
-//     let userType = user_type_dt.msg[0].type_code;
-//         let log_dt;
+    var userType = user_type_dt.msg[0].type_code;
+    var log_dt;
 
-//     if(user_type_dt.suc > 0 && user_type_dt.msg.length > 0){
-//        if(user_type_dt.msg[0].type_code == '1' && user_type_dt.msg[0].type_code == '2' && user_type_dt.msg[0].type_code == '5'){
-//           //login app
-//           var log_dt = await app_login_data(data);
-//           // console.log(log_dt);
-//        }else {
-//           var log_dt = await superadmin_login_data(data);
-//        }
-//     }
    
-//     if (log_dt.suc > 0) {
-//         if (log_dt.msg.length > 0) {
-//           if (await bcrypt.compare(data.password.toString(), log_dt.msg[0].password)) {
-//             try{
-//                 await db_Insert('md_user', `created_by = "${data.emp_id}", created_at="${datetime}"`, null, `emp_id='${log_dt.msg[0].emp_id}'`, 1)
-//             }catch (error) {
-//                 console.log(err);
-//             }
-//             res.send({ suc: 1, msg: `${log_dt.msg[0].user_type} Login successfully`, user_dtls: log_dt.msg[0] });
-//           } else {
-//             result = {
-//               suc: 0,
-//               msg: "Please check your userid or password",
-//             };
-//             res.send(result)
-//           }
-//           } else {
-//             result = { suc: 2, msg: "No data found", dt: log_dt };
-//             res.send(result)
-//           }
-//         }  else {
-//           result = { suc: 0, msg: log_dt.msg, dt: log_dt };
-//           res.send(result)
-//         }
-//   });
+       if(userType == '1' && userType == '2' && userType == '5'){
+          //login app
+          var log_dt = await app_login_data(data);
+          // console.log(log_dt);
+       }else {
+          var log_dt = await superadmin_login_data(data);
+       }
+   
+    if (log_dt.suc > 0) {
+        if (log_dt.msg.length > 0) {
+          if (await bcrypt.compare(data.password.toString(), log_dt.msg[0].password)) {
+            try{
+                await db_Insert('md_user', `created_by = "${data.emp_id}", created_at="${datetime}"`, null, `emp_id='${log_dt.msg[0].emp_id}'`, 1)
+            }catch (error) {
+                console.log(err);
+            }
+            res.send({ suc: 1, msg: `${log_dt.msg[0].user_type} Login successfully`, user_dtls: log_dt.msg[0] });
+          } else {
+            result = {
+              suc: 0,
+              msg: "Please check your userid or password",
+            };
+            res.send(result)
+          }
+          } else {
+            result = { suc: 2, msg: "No data found", dt: log_dt };
+            res.send(result)
+          }
+        }  else {
+          result = { suc: 0, msg: log_dt.msg, dt: log_dt };
+          res.send(result)
+        }
+      } catch (error) {
+        console.error("Login Error:", error);
+        res.status(500).send({ suc: 0, msg: "Internal Server Error", error: error.message });
+    }
+  });
 
 userRouter.post('/login_app', async (req, res) => {
     var data = req.body,
