05fdd8f05cefebfe5c41d51d66ab0f0bf6c7472c
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-05 10:26:18 +0530

    User Menu List 2025.02.04

--- router/admin/loanRouter.js
+++ router/admin/loanRouter.js
@@ -24,7 +24,7 @@ loanRouter.post("/fetch_loan_application_dtls", async (req, res) => {
     //fetch loan application details
         var select = "a.branch_code,a.member_code,a.client_name,b.form_no,DATE_FORMAT(b.grt_date, '%Y-%m-%d') application_date,b.prov_grp_code,DATE_FORMAT(b.approved_at, '%Y-%m-%d') grt_approve_date,c.branch_name,d.group_name,d.acc_no1,d.acc_no2,e.applied_amt,e.loan_purpose,e.sub_pupose,f.purpose_id,g.sub_purp_name",
         table_name = "md_member a LEFT JOIN td_grt_basic b ON a.member_code = b.member_code LEFT JOIN md_branch c ON a.branch_code = c.branch_code LEFT JOIN md_group d ON b.prov_grp_code = d.group_code LEFT JOIN td_grt_occupation_household e ON a.branch_code = e.branch_code AND b.form_no = e.form_no LEFT JOIN md_purpose f ON e.loan_purpose = f.purp_id LEFT JOIN md_sub_purpose g ON e.sub_pupose = g.sub_purp_id",
-        whr = `a.branch_code = '${data.branch_code}' AND a.member_code like '%${data.member_dtls}%' OR a.client_name like '%${data.member_dtls}%' OR a.client_mobile like '%${data.member_dtls}%' OR b.form_no like '%${data.member_dtls}%'`,
+        whr = `a.branch_code = '${data.branch_code}' AND (a.member_code like '%${data.member_dtls}%' OR a.client_name like '%${data.member_dtls}%' OR a.client_mobile like '%${data.member_dtls}%' OR b.form_no like '%${data.member_dtls}%')`,
         order = null;
         var fetch_appl_dtls = await db_Select(select,table_name,whr,order);
    
