835e2b3ede6ef2a7c73389e01832587f29825bef
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-13 12:50:07 +0530

    rectify disbursement screen 13.02.2025

--- router/admin/loanRouter.js
+++ router/admin/loanRouter.js
@@ -66,11 +66,10 @@ loanRouter.post("/fetch_appl_dtls_via_grp", async (req, res) => {
 loanRouter.post("/fetch_disb_trans_dtls", async (req, res) => {
   var data = req.body;
 
-  var select = "a.scheme_id,a.fund_id,a.period,a.curr_roi,a.recovery_day,a.period_mode,b.scheme_name,c.fund_name",
-  table_name = "td_loan a LEFT JOIN md_scheme b ON a.scheme_id = b.scheme_id LEFT JOIN md_fund c ON a.fund_id = c.fund_id",
+  var select = "a.scheme_id,a.fund_id,a.period,a.curr_roi,a.recovery_day,a.period_mode,b.bank_charge,b.proc_charge,b.bank_name code,c.scheme_name,d.fund_name,e.bank_name",
+  table_name = "td_loan a JOIN td_loan_transactions b ON a.loan_id = b.loan_id LEFT JOIN md_scheme c ON a.scheme_id = c.scheme_id LEFT JOIN md_fund d ON a.fund_id = d.fund_id LEFT JOIN md_bank e ON b.bank_name = e.bank_code",
   whr = `a.branch_code = '${data.branch_code}' AND a.group_code = '${data.group_code}'`,
-  order = `GROUP BY a.scheme_id, a.fund_id, a.period, a.curr_roi, a.recovery_day, a.period_mode, 
-  b.scheme_name, c.fund_name`;
+  order = `a.scheme_id,a.fund_id,a.period,a.curr_roi,a.recovery_day,a.period_mode,b.bank_charge,b.proc_charge,b.bank_name code,c.scheme_name,d.fund_name,e.bank_name`;
   var fetch_disb_dtls = await db_Select(select,table_name,whr,order);
 
   res.send(fetch_disb_dtls);
