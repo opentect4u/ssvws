c69b6314588927477c3d67e997a037b69f2d02ea
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-19 18:09:45 +0530

    dong fetch group 19.02.2025

--- modules/admin/fetchModule.js
+++ modules/admin/fetchModule.js
@@ -117,44 +117,73 @@ module.exports = {
       },
     
       edit_household_dt_web: (data) => {
-        return new Promise(async (resolve, reject) => {
-          try{
-          let datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+  //       return new Promise(async (resolve, reject) => {
+  //         try{
+  //         let datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
-          var select = "form_no",
-            table_name = "td_grt_occupation_household",
-            whr = `form_no = '${data.form_no}'`,
-            order = null;
-            var res_dt = await db_Select(select,table_name,whr,order)
+  //         var select = "form_no",
+  //           table_name = "td_grt_occupation_household",
+  //           whr = `form_no = '${data.form_no}'`,
+  //           order = null;
+  //           var res_dt = await db_Select(select,table_name,whr,order)
     
-            if(res_dt.msg.length > 0 && res_dt.suc > 0){  
-          var table_name = "td_grt_occupation_household",
-            fields = `no_of_rooms = '${data.no_of_rooms}', house_type = '${data.house_type}', own_rent = '${data.own_rent}', land = '${data.land == '' ? 0 : data.land}', tv_flag = '${data.tv_flag}', 
-                    bike_flag = '${data.bike_flag}', fridge_flag = '${data.fridge_flag}', wm_flag = '${data.wm_flag}', poltical_flag = '${data.poltical_flag}',
-                 parental_addr = '${data.parental_addr.split("'").join("\\'")}', parental_phone = '${data.parental_phone > 0 ? data.parental_phone : 0}', modified_by = '${data.modified_by}', modified_at = '${datetime}'`,
-            values = null,
-            whr = `form_no = '${data.form_no}'`,
-            flag = 1;
-          var edit_household_dt = await db_Insert(
-            table_name,
-            fields,
-            values,
-            whr,
-            flag
-          );
-        }else {
-          var table_name = "td_grt_occupation_household",
-          fields = `(no_of_rooms, house_type, own_rent, land, tv_flag, bike_flag, fridge_flag, wm_flag,poltical_flag,parental_addr,parental_phone,created_by,created_at)`,
-          values = `('${data.no_of_rooms}', '${data.house_type}', '${data.own_rent}', '${data.land == '' ? 0 : data.land}', '${data.tv_flag}', '${data.bike_flag}', '${data.fridge_flag}', '${data.wm_flag}','${data.poltical_flag}', '${data.parental_addr.split("'").join("\\'")}', '${data.parental_phone == '' ? 0 : data.parental_phone}', '${data.created_by}', ${datetime})`,
-          whr = null,
-          flag = 0;
-          var edit_household_dt = await db_Insert(table_name, fields, values, whr, flag);
-  }
-          resolve(edit_household_dt);
-        } catch (error) {
-          reject(error);
-      }
-        });
+  //           if(res_dt.msg.length > 0 && res_dt.suc > 0){  
+  //         var table_name = "td_grt_occupation_household",
+  //           fields = `no_of_rooms = '${data.no_of_rooms}', house_type = '${data.house_type}', own_rent = '${data.own_rent}', land = '${data.land == '' ? 0 : data.land}', tv_flag = '${data.tv_flag}', 
+  //                   bike_flag = '${data.bike_flag}', fridge_flag = '${data.fridge_flag}', wm_flag = '${data.wm_flag}', poltical_flag = '${data.poltical_flag}',
+  //                parental_addr = '${data.parental_addr.split("'").join("\\'")}', parental_phone = '${data.parental_phone > 0 ? data.parental_phone : 0}', modified_by = '${data.modified_by}', modified_at = '${datetime}'`,
+  //           values = null,
+  //           whr = `form_no = '${data.form_no}'`,
+  //           flag = 1;
+  //         var edit_household_dt = await db_Insert(
+  //           table_name,
+  //           fields,
+  //           values,
+  //           whr,
+  //           flag
+  //         );
+  //       }else {
+  //         var table_name = "td_grt_occupation_household",
+  //         fields = `(no_of_rooms, house_type, own_rent, land, tv_flag, bike_flag, fridge_flag, wm_flag,poltical_flag,parental_addr,parental_phone,created_by,created_at)`,
+  //         values = `('${data.no_of_rooms}', '${data.house_type}', '${data.own_rent}', '${data.land == '' ? 0 : data.land}', '${data.tv_flag}', '${data.bike_flag}', '${data.fridge_flag}', '${data.wm_flag}','${data.poltical_flag}', '${data.parental_addr.split("'").join("\\'")}', '${data.parental_phone == '' ? 0 : data.parental_phone}', '${data.created_by}', ${datetime})`,
+  //         whr = null,
+  //         flag = 0;
+  //         var edit_household_dt = await db_Insert(table_name, fields, values, whr, flag);
+  // }
+  //         resolve(edit_household_dt);
+  //       } catch (error) {
+  //         reject(error);
+  //     }
+  //       });
+
+   return new Promise(async (resolve, reject) => {
+              let datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+  
+              var select = "form_no",
+              table_name = "td_grt_occupation_household",
+              whr = `form_no = '${data.form_no}'`,
+              order = null;
+              var res_dt = await db_Select(select,table_name,whr,order)
+  
+              if(res_dt.msg.length > 0 && res_dt.suc > 0){
+                  var table_name = "td_grt_occupation_household",
+                      fields = `no_of_rooms = '0', house_type = ${data.house_type ? `'${data.house_type}'` : 'NULL'}, own_rent = ${data.own_rent ? `'${data.own_rent}'` : 'NULL'}, land = '${data.land == '' ? 0 : data.land}', tv_flag = ${data.tv_flag ? `'${data.tv_flag}'` : 'NULL'}, 
+                      bike_flag = ${data.bike_flag ? `'${data.bike_flag}'` : 'NULL'}, fridge_flag = ${data.fridge_flag ? `'${data.fridge_flag}'` : 'NULL'}, wm_flag = ${data.wm_flag ? `'${data.wm_flag}'` : 'NULL'}, poltical_flag = ${data.political_flag ? `'${data.political_flag}'` : 'NULL'},
+                   parental_addr = '${data.parental_addr.split("'").join("\\'")}', parental_phone = '${data.parental_phone == '' ? 0 : data.parental_phone}', modified_by = '${data.modified_by}', modified_at = '${datetime}'`,
+                      values = null,
+                      whr = `form_no = '${data.form_no}' AND branch_code = '${data.branch_code}'`,
+                      flag = 1;
+                      var house_hold_dt = await db_Insert(table_name, fields, values, whr, flag);
+              }else {
+                  var table_name = "td_grt_occupation_household",
+                      fields = `(no_of_rooms, house_type, own_rent, land, tv_flag, bike_flag, fridge_flag, wm_flag,poltical_flag,parental_addr,parental_phone,created_by,created_at)`,
+                      values = `('0', ${data.house_type ? `'${data.house_type}'` : 'NULL'}, ${data.own_rent ? `'${data.own_rent}'` : 'NULL'}, '${data.land == '' ? 0 : data.land}', ${data.tv_flag ? `'${data.tv_flag}'` : 'NULL'}, ${data.bike_flag ? `'${data.bike_flag}'` : 'NULL'}, ${data.fridge_flag ? `'${data.fridge_flag}'` : 'NULL'}, ${data.wm_flag ? `'${data.wm_flag}'` : 'NULL'},${data.political_flag ? `'${data.political_flag}'` : 'NULL'}, '${data.parental_addr.split("'").join("\\'")}', '${data.parental_phone == '' ? 0 : data.parental_phone}', '${data.created_by}', ${datetime})`,
+                      whr = null,
+                      flag = 0;
+                      var house_hold_dt = await db_Insert(table_name, fields, values, whr, flag);
+              }
+              resolve(house_hold_dt)
+          });
       },
 
       edit_family_dt_web: (data) => {
