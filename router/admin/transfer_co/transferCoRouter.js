aa7f9466d50019b0cb93c0d2b1f3e7420c60f92e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-21 21:57:44 +0530

    start work on grouwise co transfer 21.02.2025

--- router/admin/transfer_co/transferCoRouter.js
+++ router/admin/transfer_co/transferCoRouter.js
@@ -179,7 +179,7 @@ transferCoRouter.post("/transfer_co_view_all_details", async (req, res) => {
     if(data.flag == 'P'){
     var select = "a.trf_date,a.group_code,a.from_co,a.from_brn,a.to_co,a.to_brn,a.remarks,a.approval_status,b.group_name,c.emp_name from_co_name,d.emp_name to_co_name,e.branch_name from_brn_name,f.branch_name to_brn_name",
     table_name = "td_co_transfer a LEFT JOIN md_group b ON a.group_code = b.group_code LEFT JOIN md_employee c ON a.from_co = c.emp_id LEFT JOIN md_employee d ON a.to_co = d.emp_id LEFT JOIN md_branch e ON a.from_brn = e.branch_code LEFT JOIN md_branch f ON a.to_brn = f.branch_code",
-    whr = `a.group_code = '${data.group_code}' AND a.approval_status = 'U'`,
+    whr = `a.group_code = '${data.group_code}' AND from_co = '${data.from_co}' AND a.approval_status = 'U'`,
     order = null;
     var view_co_trans_dt = await db_Select(select,table_name,whr,order);
    
@@ -187,7 +187,7 @@ transferCoRouter.post("/transfer_co_view_all_details", async (req, res) => {
     }else {
         var select = "a.trf_date,a.group_code,a.from_co,a.from_brn,a.to_co,a.to_brn,a.remarks,a.approval_status,b.group_name,c.emp_name from_co_name,d.emp_name to_co_name,e.branch_name from_brn_name,f.branch_name to_brn_name",
         table_name = "td_co_transfer a LEFT JOIN md_group b ON a.group_code = b.group_code LEFT JOIN md_employee c ON a.from_co = c.emp_id LEFT JOIN md_employee d ON a.to_co = d.emp_id LEFT JOIN md_branch e ON a.from_brn = e.branch_code LEFT JOIN md_branch f ON a.to_brn = f.branch_code",
-        whr = `a.group_code = '${data.group_code}' AND a.approval_status = 'A'`,
+        whr = `a.group_code = '${data.group_code}' AND from_co = '${data.from_co}' AND a.approval_status = 'A'`,
         order = null;
         var view_co_trans_dt = await db_Select(select,table_name,whr,order); 
         res.send(view_co_trans_dt)
