25dbfcb09c8d22ad23b839407bb7feb2c8afc59e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-24 16:33:21 +0530

    start work on grouwise co transfer 24.02.2025

--- modules/admin/fetchModule.js
+++ modules/admin/fetchModule.js
@@ -25,13 +25,9 @@ module.exports = {
 
             if(edit_grp_dtls.suc > 0 && edit_grp_dtls.msg.length > 0){
               if (data.grp_memberdtls.length > 0) {
-                let assignedCount = 0; 
                 for (let dt of data.grp_memberdtls) {
-                  if (data.group_code == 0 && assignedCount >= 1) {
-                    break;
-                }
                   var table_name = "td_grt_basic",
-                  fields = `prov_grp_code = '${group_code}', modified_by = '${data.modified_by}', modified_at = '${datetime}'`,
+                  fields = `prov_grp_code = '${edit_grp_dtls.msg[0].group_code}', modified_by = '${data.modified_by}', modified_at = '${datetime}'`,
                   values = null,
                   whr = `form_no = '${dt.form_no}' AND branch_code = '${data.branch_code}' AND member_code = '${dt.member_code}'`,
                   flag = 1;
