9e6c3373a511415eb91e3073cc5c5b859a2513f0
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-25 08:41:20 +0530

    start work on menu 25.02.2025

--- router/admin/menu/menu_permissionRouter.js
+++ router/admin/menu/menu_permissionRouter.js
@@ -1,4 +1,4 @@
-const { db_Select } = require('../../../model/mysqlModel');
+const { db_Select, db_Insert } = require('../../../model/mysqlModel');
 
 const express = require('express'),
 menu_permissionRouter = express.Router(),
@@ -13,6 +13,33 @@ menu_permissionRouter.post("/fetch_menu_permission_dtls", async (req, res) => {
   order = null;
   var menu_dt = await db_Select(select,table_name,whr,order);
   res.send(menu_dt)
+});
+
+menu_permissionRouter.post("/menu_permission", async (req, res) => {
+    try{
+    let datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+    var data = req.body;
+
+    var select = "COUNT(user_type) sl_no",
+    table_name = "td_menu_permission",
+    whr = `user_type = '${data.user_type}'`,
+    order = null;
+    var count_user_type = await db_Select(select,table_name,whr,order);
+
+    if(count_user_type.suc > 0 && count_user_type.msg.length > 0){
+        var data = req.body;
+
+        var table_name = "td_menu_permission",
+        fields = data.user_type > 0 ? `grt = '${data.grt}', applications = '${data.applications}', search_member = '${data.search_member}',search_member = '${data.search_member}', groups = '${data.groups}', edit_group = '${data.edit_group}', add_group = '${data.add_group}', transfer_group = '${data.transfer_group}', approve_group_transfer = '${data.approve_group_transfer}', view_group_transfer = '${data.view_group_transfer}', attendance = '${data.attendance}', attendance_dashboard = '${data.attendance_dashboard}', loans = '${data.loans}', disburse_loan = '${data.disburse_loan}', view_loan = '${data.view_loan}', approve_transaction = '${data.approve_transaction}', reports = '${data.reports}', loan_statement = '${data.loan_statement}', loan_transactions = '${data.loan_transactions}', demand_report = '${data.demand_report}', outstanding_report = '${data.outstanding_report}', fundwise_report = '${data.fundwise_report}', schemewise_report = '${data.schemewise_report}', demand_vs_collection = '${data.demand_vs_collection}', master = '${data.master}', banks = '${data.banks}', employees = '${data.employees}', designation = '${data.designation}', user_management = '${data.user_management}', create_user = '${data.create_user}', manage_user = '${data.manage_user}', transfer_user = '${data.transfer_user}', modified_by = '${data.modified_by}', modified_at = '${datetime}'` : `(user_type,grt,applications,search_member,groups,edit_group,add_group,transfer_group,approve_group_transfer,view_group_transfer,attendance,attendance_dashboard,loans,disburse_loan,view_loan,approve_transaction,reports,loan_statement,loan_transactions,demand_report,outstanding_report,fundwise_report,schemewise_report,demand_vs_collection,master,banks,employees,designation,user_management,create_user,manage_user,transfer_user,created_by,created_at)`,
+        values = `('${data.user_type}','${data.grt}','${data.applications}','${data.search_member}','${data.groups}','${data.edit_group}','${data.add_group}','${data.transfer_group}','${data.approve_group_transfer}','${data.view_group_transfer}','${data.attendance}','${data.attendance_dashboard}','${data.loans}','${data.disburse_loan}','${data.view_loan}','${data.approve_transaction}','${data.reports}','${data.loan_statement}','${data.loan_transactions}','${data.demand_report}','${data.outstanding_report}','${data.fundwise_report}','${data.schemewise_report}','${data.demand_vs_collection}','${data.master}','${data.banks}','${data.employees}','${data.designation}','${data.user_management}','${data.create_user}','${data.manage_user}','${data.transfer_user}','${data.created_by}','${datet}')`,
+        whr = data.user_type > 0 ? `user_type = '${data.user_type}'` : null,
+        flag = data.user_type > 0 ? 1 : 0;
+        var menu_permission_data = await db_Insert(table_name,fields,values,whr,flag);
+    }
+    res.send({"suc" : 1, "msg":"menu permission solved successfully", menu_permission_data})
+  } catch (error) {
+    reject(error);
+  }
 });
 
 module.exports = {menu_permissionRouter}
