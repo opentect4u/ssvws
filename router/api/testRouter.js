5191bd03553ddbd96fdd0bc5bee7195444c62c03
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-02-27 19:18:39 +0530

    commit group policy 27.02.2025

--- router/api/testRouter.js
+++ router/api/testRouter.js
@@ -1,3 +1,4 @@
+const { emp_id } = require('../../modules/admin/employee/empModule');
 const { getMonthDiff, getLoanDmd } = require('../../modules/api/masterModule');
 
 const express = require('express'),
@@ -11,6 +12,15 @@ testRouter.get("/test", async (req, res) => {
     res.send(get_data);
     // console.log(get_data,'ok');
     
-})
+});
+
+testRouter.get("/test_1", async (req, res) => {
+    var data = req.query;
+
+    var gets_data = await emp_id(120)
+    res.send(gets_data);
+    // console.log(get_data,'ok');
+    
+});
 
 module.exports = {testRouter}
