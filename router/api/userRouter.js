391de578d4bfe30c58d7a94dbf769157dc8cc314
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-03 13:07:43 +0530

    comment out log section 03.03.2025

--- router/api/userRouter.js
+++ router/api/userRouter.js
@@ -71,7 +71,7 @@ userRouter.post('/login_app', async (req, res) => {
       app_data = await db_Select("version","md_app_version",null,null);
       if (app_data && app_data.length > 0) {
         var app_data_inf = app_data[0].version; // Extract version number from the first row
-        console.log(app);
+        console.log(app_data_inf);
         
     } else {
         return res.send({ suc: 0, msg: "App version information not found." });
