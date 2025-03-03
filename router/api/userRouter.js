dd8e477664b821a92a9bee0800358875491ff9f1
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-03 13:48:06 +0530

    comment out log section 03.03.2025

--- router/api/userRouter.js
+++ router/api/userRouter.js
@@ -71,10 +71,10 @@ userRouter.post('/login_app', async (req, res) => {
       // Fetch app version if flag is 'A'
       if (data.flag === 'A') {
           let app_data = await db_Select("version", "md_app_version", null, null);
-          console.log(app_data,app_data[0].version,'poii');
+          console.log(app_data,'poii');
           
-          if (Array.isArray(app_data) && app_data.length > 0) {
-            requiredVersion = app_data.version; // Extract version
+          if (app_data.suc > 0 && app_data.msg.length > 0) {
+            requiredVersion = app_data.msg[0].version; // Extract version
         } else {
             return res.send({ suc: 0, msg: "App version information not found.", requiredVersion });
         }
