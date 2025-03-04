7af26e3435b779e71b78be56e1ebcba47e340bfb
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-04 16:29:53 +0530

    start work in view loan section 04.03.2025

--- modules/admin/loanModule.js
+++ modules/admin/loanModule.js
@@ -160,7 +160,7 @@ module.exports = {
             let datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
             var table_name = "td_loan",
-            fields = `purpose = '${data.purpose}', sub_purpose = '${data.sub_purpose}', fund_id = '${data.fund_id}', tot_emi = '${data.tot_emi}', modified_by = '${data.modified_by}', modified_dt = '${datetime}'`,
+            fields = `purpose = '${data.purpose}', disb_dt = '${data.disb_dt}', modified_by = '${data.modified_by}', modified_dt = '${datetime}'`,
             values = null,
             whr = `loan_id = '${data.loan_id}'`,
             flag = 1;
