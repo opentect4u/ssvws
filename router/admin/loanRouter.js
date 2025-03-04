2770b7520b2cb847c47d95d084300b18516a12a0
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-04 16:11:39 +0530

    start work in view loan section 04.03.2025

--- router/admin/loanRouter.js
+++ router/admin/loanRouter.js
@@ -612,10 +612,10 @@ loanRouter.post("/view_loan_dtls", async (req, res) => {
 
   //view loan details
   var select =
-      "a.loan_id,a.branch_code,a.group_code,a.member_code,a.grt_form_no,a.purpose,a.applied_amt,a.applied_dt,a.scheme_id,a.fund_id,a.period,a.curr_roi,a.disb_dt,a.prn_disb_amt,a.intt_cal_amt,a.prn_amt,a.intt_amt,a.outstanding,a.prn_emi,a.intt_emi,a.tot_emi,a.recovery_day,a.period_mode,a.instl_paid,a.instl_start_dt,a.instl_end_dt,a.last_trn_dt,b.client_name,c.purpose_id,e.scheme_name,f.fund_name,g.group_name",
+      "a.loan_id,a.branch_code,a.group_code,g.group_name,a.member_code,b.client_name,a.purpose,c.purpose_id,a.scheme_id,e.scheme_name,a.period_mode,a.fund_id,f.fund_name,a.period,a.curr_roi,a.disb_dt,a.prn_disb_amt,a.intt_cal_amt,a.prn_amt,a.intt_amt,a.outstanding,a.prn_emi,a.intt_emi,a.tot_emi,a.recovery_day",
     table_name =
-      "td_loan a LEFT JOIN md_member b ON a.branch_code = b.branch_code AND a.member_code = b.member_code LEFT JOIN md_purpose c ON a.purpose = c.purp_id LEFT JOIN md_scheme e ON a.scheme_id = e.scheme_id LEFT JOIN md_fund f ON a.fund_id = f.fund_id LEFT JOIN md_group g ON a.group_code = g.group_code",
-    whr = `a.loan_id = '${data.loan_id}'`,
+      "td_loan a LEFT JOIN md_member b ON a.member_code = b.member_code LEFT JOIN md_purpose c ON a.purpose = c.purp_id LEFT JOIN md_scheme e ON a.scheme_id = e.scheme_id LEFT JOIN md_fund f ON a.fund_id = f.fund_id LEFT JOIN md_group g ON a.group_code = g.group_code",
+    whr = `a.loan_id = '${data.loan_id}' AND branch_code = '${data.branch_code}'`,
     order = null;
   var loan_dtls = await db_Select(select, table_name, whr, order);
 
