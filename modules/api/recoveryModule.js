ddafbd847fcdca7950435913f48000bfdaf6ff69
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-05 19:47:06 +0530

    work on grt approve & rejection 05.03.2025

--- modules/api/recoveryModule.js
+++ modules/api/recoveryModule.js
@@ -283,6 +283,7 @@ module.exports = {
     recovery_trans: (data) => {
         return new Promise(async (resolve, reject) => {
             let datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+            var trans_dtl = {suc:0, msg: ''}
             try{
 
             if(data.recovdtls.length > 0){
@@ -298,8 +299,6 @@ module.exports = {
                     let intt_balance = parseFloat(dt.intt_amt) //previoud intt
                     let inttEMI = parseFloat(dt.intt_emi)      //intt recovery
 
-                    
-
                     if(prnEmi > balance){
                         let excess_amt = prnEmi - balance
                         prnEmi = balance
@@ -313,77 +312,79 @@ module.exports = {
                     let intt_recovs =  intt_balance -  inttEMI
 
                     if(dt.credit > 0 && dt.credit){ 
-                    let payment_id = await payment_code()     //interest
-                    console.log(payment_id,'id_intt');
- 
-                    var table_name = "td_loan_transactions",
-                    fields = `(payment_date,payment_id,branch_id,loan_id,particulars,credit,debit,balance,intt_balance,tr_type,tr_mode,status,created_by,created_at,trn_lat,trn_long,trn_addr)`,
-                    values = `('${dateFormat(dt.last_trn_dt, "yyyy-mm-dd")}','${payment_id}','${data.branch_code == '' ? 0 : data.branch_code}','${dt.loan_id}','To Interest','0','${inttEMI}','${balance > 0 ? balance : 0}','${intt_balance}','I','${data.tr_mode}','U','${data.created_by}','${datetime}','${data.trn_lat}','${data.trn_long}','${data.trn_addr.split("'").join("\\'")}')`,
-                    whr = null,
-                    flag = 0;
-                    var rec_dtls_int = await db_Insert(table_name,fields,values,whr,flag);
-                    console.log(rec_dtls_int);
-
+                        let payment_id = await payment_code()     //interest
+                        console.log(payment_id,'id_intt');
     
-                    if(rec_dtls_int.suc > 0 && rec_dtls_int.msg.length > 0){
-                      payment_id = await payment_code()
-                        console.log(payment_id,'id_recov');
-                        
                         var table_name = "td_loan_transactions",
-                        fields = `(payment_date,payment_id,branch_id,loan_id,particulars,credit,debit,prn_recov,intt_recov,balance,od_balance,intt_balance,recov_upto,tr_type,tr_mode,bank_name,cheque_id,chq_dt,status,created_by,created_at,trn_lat,trn_long,trn_addr)`,
-                        values = `('${dateFormat(dt.last_trn_dt, "yyyy-mm-dd")}','${payment_id}','${data.branch_code == '' ? 0 : data.branch_code}','${dt.loan_id}','${data.tr_mode == 'B' ? 'By UPI [Installment Payment]' : data.tr_mode == 'C' ? 'By Cash [Installment Payment]' : ''}','${dt.credit}','0','${prnEmi > 0 ?prnEmi : 0}','${inttEMI > 0 ? inttEMI : 0}','${prn_recov > 0 ? prn_recov : 0}','0','${intt_recovs > 0 ? intt_recovs : 0}','${datetime}','R','${data.tr_mode}','${data.bank_name}','${data.cheque_id == '' ? 0 : data.cheque_id}', '${data.chq_dt == '' ? null : data.chq_dt}','U','${data.created_by}','${datetime}','${data.trn_lat}','${data.trn_long}','${data.trn_addr.split("'").join("\\'")}')`,
+                        fields = `(payment_date,payment_id,branch_id,loan_id,particulars,credit,debit,balance,intt_balance,tr_type,tr_mode,status,created_by,created_at,trn_lat,trn_long,trn_addr)`,
+                        values = `('${dateFormat(dt.last_trn_dt, "yyyy-mm-dd")}','${payment_id}','${data.branch_code == '' ? 0 : data.branch_code}','${dt.loan_id}','To Interest','0','${inttEMI}','${balance > 0 ? balance : 0}','${intt_balance}','I','${data.tr_mode}','U','${data.created_by}','${datetime}','${data.trn_lat}','${data.trn_long}','${data.trn_addr.split("'").join("\\'")}')`,
                         whr = null,
                         flag = 0;
-                        var rec_dtls_prn = await db_Insert(table_name,fields,values,whr,flag);  
-                         console.log(rec_dtls_prn);
+                        var rec_dtls_int = await db_Insert(table_name,fields,values,whr,flag);
+                        console.log(rec_dtls_int);
 
-                    }
+        
+                        if(rec_dtls_int.suc > 0){
+                            payment_id = await payment_code()
+                            console.log(payment_id,'id_recov');
+                            
+                            var table_name = "td_loan_transactions",
+                            fields = `(payment_date,payment_id,branch_id,loan_id,particulars,credit,debit,prn_recov,intt_recov,balance,od_balance,intt_balance,recov_upto,tr_type,tr_mode,bank_name,cheque_id,chq_dt,status,created_by,created_at,trn_lat,trn_long,trn_addr)`,
+                            values = `('${dateFormat(dt.last_trn_dt, "yyyy-mm-dd")}','${payment_id}','${data.branch_code == '' ? 0 : data.branch_code}','${dt.loan_id}','${data.tr_mode == 'B' ? 'By UPI [Installment Payment]' : data.tr_mode == 'C' ? 'By Cash [Installment Payment]' : ''}','${dt.credit}','0','${prnEmi > 0 ?prnEmi : 0}','${inttEMI > 0 ? inttEMI : 0}','${prn_recov > 0 ? prn_recov : 0}','0','${intt_recovs > 0 ? intt_recovs : 0}','${datetime}','R','${data.tr_mode}','${data.bank_name}','${data.cheque_id == '' ? 0 : data.cheque_id}', '${data.chq_dt == '' ? null : data.chq_dt}','U','${data.created_by}','${datetime}','${data.trn_lat}','${data.trn_long}','${data.trn_addr.split("'").join("\\'")}')`,
+                            whr = null,
+                            flag = 0;
+                            var rec_dtls_prn = await db_Insert(table_name,fields,values,whr,flag);  
+                            console.log(rec_dtls_prn);
+
+                            if(rec_dtls_prn.suc > 0 && rec_dtls_prn.msg.length > 0){
+                            
+                                let prn_update  =  balance - prnEmi;
+                                let intt_update =  intt_balance -  inttEMI
+                                let outs_update =  parseFloat(prn_update) + parseFloat(intt_update)
+                                
+                                // console.log(intt_update,'intt_recovery');
+                                
+                                let outstanding = parseFloat(prn_recov) + parseFloat(intt_update);
+            
+                                // console.log(prn_recov,intt_recov,outstanding,'calculate');
+                                
                 
+                                var table_name = "td_loan",
+                                fields = `prn_amt = '${prn_update}', intt_amt = '${intt_update}', outstanding = '${outs_update}', instl_paid = '${dt.instl_paid}', last_trn_dt = '${dt.last_trn_dt}', modified_by = '${data.modified_by}', modified_dt = '${datetime}'`,
+                                values = null,
+                                whr = `loan_id = '${dt.loan_id}'`,
+                                flag = 1;
+                                var rec_dt = await db_Insert(table_name,fields,values,whr,flag);
+                                console.log(rec_dt);
+                            
+                                if(rec_dt.suc > 0 && rec_dt.msg.length > 0){
         
-                    if(rec_dtls_prn.suc > 0 && rec_dtls_prn.msg.length > 0){
+                                    var select = `a.loan_id,a.member_code,a.branch_code,a.group_code,b.payment_date tnx_date,b.tr_mode,b.cheque_id,b.credit,b.created_by collec_code,c.group_name,d.branch_name,e.emp_name collec_name,f.client_name,(
+                                    SELECT SUM(i.outstanding)
+                                    FROM td_loan i
+                                    WHERE i.group_code = '${data.group_code}' AND b.tr_type = 'R'
+                                    ) outstanding`,
+                                    table_name = "td_loan a JOIN td_loan_transactions b ON a.loan_id = b.loan_id AND a.branch_code = b.branch_id JOIN md_group c ON a.branch_code = c.branch_code AND a.group_code = c.group_code JOIN md_branch d ON a.branch_code = d.branch_code JOIN md_employee e ON b.created_by = e.emp_id JOIN md_member f ON a.branch_code = f.branch_code AND a.member_code = f.member_code",
+                                    whr = `a.group_code = '${data.group_code}' AND b.tr_type = 'R' AND b.created_at = 
+                                    (SELECT MAX(created_at) 
+                                                        FROM td_loan_transactions 
+                                                    WHERE loan_id IN (${dt.loan_id}))`,
+                                    order = `GROUP BY a.loan_id,a.member_code,a.branch_code,a.group_code,b.payment_date,b.credit,b.tr_mode,b.cheque_id,b.created_by,c.group_name,d.branch_name,e.emp_name,f.client_name`;
+                                    trans_dtl = await db_Select(select,table_name,whr,order);
                     
-                    let prn_update  =  balance - prnEmi;
-                    let intt_update =  intt_balance -  inttEMI
-                    let outs_update =  parseFloat(prn_update) + parseFloat(intt_update)
-                    
-                    // console.log(intt_update,'intt_recovery');
-                    
-                    let outstanding = parseFloat(prn_recov) + parseFloat(intt_update);
-
-                    // console.log(prn_recov,intt_recov,outstanding,'calculate');
-                    
-    
-                    var table_name = "td_loan",
-                    fields = `prn_amt = '${prn_update}', intt_amt = '${intt_update}', outstanding = '${outs_update}', instl_paid = '${dt.instl_paid}', last_trn_dt = '${dt.last_trn_dt}', modified_by = '${data.modified_by}', modified_dt = '${datetime}'`,
-                    values = null,
-                    whr = `loan_id = '${dt.loan_id}'`,
-                    flag = 1;
-                    var rec_dt = await db_Insert(table_name,fields,values,whr,flag);
-                    console.log(rec_dt);
-                    
+                                    rec_dtls_prn.msg[0]['trans_dtl'] = trans_dtl.suc > 0 ? (trans_dtl.msg.length > 0 ? trans_dtl.msg : []) : [];
+                                }else{
+                                    reject({ "suc": 0, "msg": "No recovery balance provided" });
+                                }
+                            }else{
+                                reject({ "suc": 0, "msg": "No recovery balance provided" });
+                            }
+                        }else{
+                            reject({ "suc": 0, "msg": "No recovery balance provided" });
+                        }
+                    }else {
+                        reject({ "suc": 0, "msg": "No recovery balance provided" });
                     }
-    
-                    if(rec_dt.suc > 0 && rec_dt.msg.length > 0){
-
-                var select = `a.loan_id,a.member_code,a.branch_code,a.group_code,b.payment_date tnx_date,b.tr_mode,b.cheque_id,b.credit,b.created_by collec_code,c.group_name,d.branch_name,e.emp_name collec_name,f.client_name,(
-                    SELECT SUM(i.outstanding)
-                    FROM td_loan i
-                    WHERE i.group_code = '${data.group_code}' AND b.tr_type = 'R'
-                    ) outstanding`,
-                    table_name = "td_loan a JOIN td_loan_transactions b ON a.loan_id = b.loan_id AND a.branch_code = b.branch_id JOIN md_group c ON a.branch_code = c.branch_code AND a.group_code = c.group_code JOIN md_branch d ON a.branch_code = d.branch_code JOIN md_employee e ON b.created_by = e.emp_id JOIN md_member f ON a.branch_code = f.branch_code AND a.member_code = f.member_code",
-                        whr = `a.group_code = '${data.group_code}' AND b.tr_type = 'R' AND b.created_at = 
-                        (SELECT MAX(created_at) 
-                                             FROM td_loan_transactions 
-                                          WHERE loan_id IN (${dt.loan_id}))`,
-                        order = `GROUP BY a.loan_id,a.member_code,a.branch_code,a.group_code,b.payment_date,b.credit,b.tr_mode,b.cheque_id,b.created_by,c.group_name,d.branch_name,e.emp_name,f.client_name`;
-                        var trans_dtl = await db_Select(select,table_name,whr,order);
-        
-                        rec_dtls_prn.msg[0]['trans_dtl'] = trans_dtl.suc > 0 ? (trans_dtl.msg.length > 0 ? trans_dtl.msg : []) : [];
-                    
-                    }
-                }else {
-                    reject({ "suc": 0, "msg": "No recovery balance provided" });
-                }
                 }
                 resolve(trans_dtl)
             }else {
