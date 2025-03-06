5f449f89cea79a825d823f38222055b220a8c5f6
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-06 18:18:09 +0530

    work on grt approve & rejection 05.03.2025

--- router/report/branch_report/web/loan_outstandingRouter.js
+++ router/report/branch_report/web/loan_outstandingRouter.js
@@ -1,28 +1,110 @@
 const { db_Select, db_RunProcedureAndFetchData } = require('../../../../model/mysqlModel');
-const { loan_balance_outstanding, loan_od_balance_outstanding, loan_intt_balance_outstanding } = require('../../../../modules/api/masterModule');
+const { loan_balance_outstanding, loan_od_balance_outstanding, loan_intt_balance_outstanding, get_prn_amt } = require('../../../../modules/api/masterModule');
 
 const express = require('express'),
 loan_outstandingRouter = express.Router(),
 dateFormat = require('dateformat');
 
+// loan_outstandingRouter.post("/loan_outstanding_report_memberwise", async (req, res) => {
+//     try {
+//         var data = req.body;
+//         const pro_name = 'p_loan_out_standing',
+//         pro_params = `?,?`,
+//         pro_params_val = [data.branch_code, data.os_dt],
+//         sel_table_name = `tt_loan_outstanding a, md_group b, md_member c`,
+//         sel_fields = `a.loan_id, a.branch_code, a.group_code, b.group_name, a.member_code, c.client_name, a.disbursed_date disb_dt, a.current_roi curr_roi, a.period, a.period_mode, a.total_emi tot_emi, a.installment_end_date instl_end_dt, a.current_principal balance, a.overdue_principal od_balance, a.interest_amount intt_balance, (a.current_principal + a.overdue_principal + a.interest_amount) total_outstanding`,
+//         sel_whr_fields = `a.group_code=b.group_code AND a.member_code = c.member_code`,
+//         sel_whr_arr = [],
+//         sel_order = `ORDER BY a.disbursed_date`;
+//         var res_dt = await db_RunProcedureAndFetchData(pro_name, pro_params, pro_params_val, sel_fields, sel_table_name, sel_whr_fields, sel_whr_arr, sel_order);
+//         res.send(res_dt);
+//     } catch (error) {
+//         console.error("Error fetching loan outstanding report:", error);
+//         res.send({ suc: 0, msg: "An error occurred" });
+//     }
+// });
+
+// loan_outstandingRouter.post("/loan_outstanding_report_memberwise", async (req, res) => {
+//     try {
+//         var data = req.body;
+
+//         var select = "a.loan_id,a.branch_code,a.group_code,b.group_name,a.member_code,c.client_name,b.bank_name,b.acc_no2,b.co_id,d.emp_name,a.recovery_day,a.fund_id,e.fund_name,a.scheme_id,f.scheme_name,a.applied_amt,a.disb_dt,a.prn_disb_amt,(prn_amt + od_prn_amt)prn_amt,intt_amt,(prn_amt + od_prn_amt+intt_amt)outstanding",
+//         table_name = "td_loan a,md_group b,md_member c,md_employee d,md_fund e,md_scheme f",
+//         whr = ` a.group_code = b.group_code
+//                 AND a.member_code = c.member_code
+//                 AND b.co_id = d.emp_id
+//                 AND a.fund_id = e.fund_id
+//                 AND a.scheme_id = f.scheme_id
+//                 AND a.branch_code = '${data.branch_code}'
+//                 AND a.disb_dt <= '${data.get_date}'`,
+//         order = null;
+//         var res_dt = await db_Select(select,table_name,whr,order);
+
+//         if(res_dt.suc > 0 && res_dt.msg.length > 0){
+//             try {
+//                 const prnAmtResult = await get_prn_amt(loan.loan_id, data.get_date);
+//                 const inttAmtResult = await get_intt_amt(loan.loan_id, data.get_date);
+                
+//                 loan.prn_amt = prnAmtResult.suc > 0 ? prnAmtResult.msg[0].prn_amt || 0 : 0;
+//                 loan.intt_amt = inttAmtResult.suc > 0 ? inttAmtResult.msg[0].intt_balance || 0 : 0;
+//                 loan.outstanding = loan.prn_amt + loan.intt_amt;
+//             } catch (error) {
+//                 console.error(`Error fetching amounts for loan_id: ${loan.loan_id}`, error);
+//                 loan.prn_amt = 0;
+//                 loan.intt_amt = 0;
+//                 loan.outstanding = 0;
+//             }
+//         }
+//         res.send(res_dt);
+//     } catch (error) {
+//         console.error("Error fetching loan outstanding report:", error);
+//         res.send({ suc: 0, msg: "An error occurred" });
+//     }
+// });
+
 loan_outstandingRouter.post("/loan_outstanding_report_memberwise", async (req, res) => {
     try {
         var data = req.body;
-        const pro_name = 'p_loan_out_standing',
-        pro_params = `?,?`,
-        pro_params_val = [data.branch_code, data.os_dt],
-        sel_table_name = `tt_loan_outstanding a, md_group b, md_member c`,
-        sel_fields = `a.loan_id, a.branch_code, a.group_code, b.group_name, a.member_code, c.client_name, a.disbursed_date disb_dt, a.current_roi curr_roi, a.period, a.period_mode, a.total_emi tot_emi, a.installment_end_date instl_end_dt, a.current_principal balance, a.overdue_principal od_balance, a.interest_amount intt_balance, (a.current_principal + a.overdue_principal + a.interest_amount) total_outstanding`,
-        sel_whr_fields = `a.group_code=b.group_code AND a.member_code = c.member_code`,
-        sel_whr_arr = [],
-        sel_order = `ORDER BY a.disbursed_date`;
-        var res_dt = await db_RunProcedureAndFetchData(pro_name, pro_params, pro_params_val, sel_fields, sel_table_name, sel_whr_fields, sel_whr_arr, sel_order);
+
+        var select = "a.loan_id,a.branch_code,a.group_code,b.group_name,a.member_code,c.client_name,b.bank_name,b.acc_no2,b.co_id,d.emp_name,a.recovery_day,a.fund_id,e.fund_name,a.scheme_id,f.scheme_name,a.applied_amt,a.disb_dt,a.prn_disb_amt",
+            table_name = "td_loan a,md_group b,md_member c,md_employee d,md_fund e,md_scheme f",
+            whr = ` a.group_code = b.group_code
+                    AND a.member_code = c.member_code
+                    AND b.co_id = d.emp_id
+                    AND a.fund_id = e.fund_id
+                    AND a.scheme_id = f.scheme_id
+                    AND a.branch_code = '${data.branch_code}'
+                    AND a.disb_dt <= '${data.get_date}'`,
+            order = null;
+
+        var res_dt = await db_Select(select, table_name, whr, order);
+
+        if (res_dt.suc > 0 && res_dt.msg.length > 0) {
+            // Fetch prn_amt and intt_amt for each loan_id
+            await Promise.all(res_dt.msg.map(async (loan) => {
+                try {
+                    const prnAmtResult = await get_prn_amt(loan.loan_id, data.get_date);
+                    const inttAmtResult = await get_intt_amt(loan.loan_id, data.get_date);
+                    
+                    loan.prn_amt = prnAmtResult.suc > 0 ? prnAmtResult.msg[0].prn_amt || 0 : 0;
+                    loan.intt_amt = inttAmtResult.suc > 0 ? inttAmtResult.msg[0].intt_balance || 0 : 0;
+                    loan.outstanding = loan.prn_amt + loan.intt_amt;
+                } catch (error) {
+                    console.error(`Error fetching amounts for loan_id: ${loan.loan_id}`, error);
+                    loan.prn_amt = 0;
+                    loan.intt_amt = 0;
+                    loan.outstanding = 0;
+                }
+            }));
+        }
+
         res.send(res_dt);
     } catch (error) {
         console.error("Error fetching loan outstanding report:", error);
         res.send({ suc: 0, msg: "An error occurred" });
     }
 });
+
 
 loan_outstandingRouter.post("/loan_outstanding_report_groupwise", async (req, res) => {
     try {
