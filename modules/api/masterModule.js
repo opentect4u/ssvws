97879a599b22dfcb6b36371281d3ae3d12d30400
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-08 19:11:42 +0530

    commit 06.03.2025

--- modules/api/masterModule.js
+++ modules/api/masterModule.js
@@ -919,7 +919,9 @@ const fetch_date = (branch_code, get_dt) => {
         if (closedDate >= formDate) {
           resolve({ suc: 0, msg: "Error: formDate must be greater than closed_upto." });
         } else {
-          resolve({ suc: 1, msg: "Success! formDate is greater than closed_upto." `${closedDate}`});
+          resolve({ suc: 1, msg: `Success! formDate is greater than closed_upto ${closedDate}` });
+          console.log(closedDate,formDate,'formDate');
+          
         }
       } else {
         reject({ suc: 0, msg: "No matching record found for this branch_code." });
