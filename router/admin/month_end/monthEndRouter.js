18582d152a897116e821ea4008b3b97a0e06fa6e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-08 11:18:54 +0530

    start work on month end 08.03.2025

---
+++ router/admin/month_end/monthEndRouter.js
@@ -0,0 +1,18 @@
+const { db_Select, db_Insert } = require('../../../model/mysqlModel');
+
+const express = require('express'),
+monthEndRouter = express.Router(),
+dateFormat = require('dateformat');
+
+monthEndRouter.post("/get_branch_name_in_month_end_screen", async (req, res) => {
+  var data = req.body;
+
+  var select = "branch_code,branch_name",
+  table_name = "md_branch",
+  whr = null,
+  order = null;
+  var branch_data = await db_Select(select,table_name,whr,order);
+  res.send(branch_data);
+});
+
+module.exports = {monthEndRouter}
--- router/adminRouter.js
+++ router/adminRouter.js
@@ -2,6 +2,7 @@ const { adminuserRouter } = require('./admin/adminuserRouter');
 const { desigRouter } = require('./admin/designation/desigRouter');
 const { fetchRouter } = require('./admin/fetchRouter');
 const { loanRouter } = require('./admin/loanRouter');
+const { monthEndRouter } = require('./admin/month_end/monthEndRouter');
 const { reportwebRouter } = require('./admin/reportwebRouter');
 const { userRouter } = require('./admin/userRouter');
 
@@ -14,6 +15,7 @@ const express = require('express'),
     adminRouter.use(loanRouter);
     adminRouter.use(reportwebRouter);
     adminRouter.use(desigRouter);
+    adminRouter.use(monthEndRouter);
 
 module.exports = {adminRouter}
     
