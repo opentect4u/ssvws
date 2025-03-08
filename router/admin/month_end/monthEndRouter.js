d52279479dd6cb07276bf43bd29919d78465f56b
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-08 13:13:04 +0530

    start work on month end 08.03.2025

--- router/admin/month_end/monthEndRouter.js
+++ router/admin/month_end/monthEndRouter.js
@@ -23,24 +23,24 @@ monthEndRouter.post("/update_month_end_data", async (req, res) => {
            if (data.month_end_dt && data.month_end_dt.length > 0) {
    
                for (let dt of data.month_end_dt) {
+                var brn_code_arr = data.month_end_dt.map(pdt => `'${pdt.branch_code}'`);
    
                    var table_name = "td_month_close",
-                       fields = `close_flag = ''`,
-                       values = ``,
-                       whr = `branch_code = '${data.branch_code}'`,
+                       fields = `close_flag = 'C', closed_by = '${data.closed_by}', closed_at = '${datetime}'`,
+                       values = null,
+                       whr = `branch_code IN (${brn_code_arr.join(',')}) AND closed_upto = '${dt.closed_upto}'`,
                        flag = 1;
    
-                   var save_trans_mem_dtls = await db_Insert(table_name, fields, values, whr, flag);
+                   var month_end_details_save = await db_Insert(table_name, fields, values, whr, flag);
                }
-               res.send({ "suc": 1, "msg": "Transfer completed successfully" });
+               res.send({ "suc": 1, "msg": "Month end done successfully" });
            } else {
-               res.send({ "suc": 0, "msg": "No member details provided" });
+               res.send({ "suc": 0, "msg": "No details provided" });
            }
        } catch (error) {
            console.error(error);
            res.send({ "suc": 0, "msg": "Error occurred", details: error });
        }
-
 });
 
 module.exports = {monthEndRouter}
