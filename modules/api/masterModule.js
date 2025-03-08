e50bd7df54774daa1e7f9f36ca147e090b82c470
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-08 17:40:12 +0530

    start work on month end 08.03.2025

--- modules/api/masterModule.js
+++ modules/api/masterModule.js
@@ -919,9 +919,9 @@ const fetch_date = (branch_code, get_dt) => {
         } else {
           reject({ suc: 0, msg: "Error: formDate must be greater than closed_upto." });
         }
-      // } else {
-      //   reject({ suc: 0, msg: "No matching record found for this branch_code." });
-      // }
+      } else {
+        reject({ suc: 0, msg: "No matching record found for this branch_code." });
+      }
     } catch (error) {
       console.error("Error on fetching date:", error);
       reject(error);
