daeaab297545cd644504d9768f7c52137b7698c3
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-13 16:17:23 +0530

    start working on outstanding report 13.03.2025

--- router/report/branch_report/web/loan_outstandingRouter.js
+++ router/report/branch_report/web/loan_outstandingRouter.js
@@ -80,13 +80,21 @@ dateFormat = require('dateformat');
                     var outstanding_data = await db_Select(select,table_name,whr,order);
                     res.send(outstanding_data);
                 }else {
+                    var select = "MAX(balance_date) balance_date",
+                    table_name = "td_loan_month_balance",
+                    whr = `branch_code = '${data.branch_code}' AND balance_date <= '${data.supply_date}'`,
+                    order = null;
+                    var res_dt = await db_Select(select,table_name,whr,order);
+
+                    if(res_dt.suc > 0 && res_dt.msg.length > 0){
                     var select = "b.group_code,c.group_name,c.co_id,c.bank_name,c.acc_no1,c.acc_no2,b.recovery_day,b.prn_disb_amt,SUM(a.prn_amt) prn_outstanding,SUM(a.intt_amt) intt_outstanding,SUM(a.outstanding) outstanding,d.emp_name co_name",
                     table_name = "td_loan_month_balance a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_group c ON b.group_code = c.group_code LEFT JOIN md_employee d ON c.co_id = d.emp_id",
-                    whr = `a.balance_date = '${data.supply_date}' AND b.branch_code = '${data.branch_code}'`,
+                    whr = `a.balance_date = '${res_dt.msg[0].balance_date}' AND b.branch_code = '${data.branch_code}'`,
                     order = `GROUP BY b.group_code,c.group_name,c.co_id,c.bank_name,c.acc_no1,c.acc_no2,b.recovery_day,b.prn_disb_amt,d.emp_name`;
                     var outstanding_data = await db_Select(select,table_name,whr,order);
                     res.send(outstanding_data);
                 }
+              }
             } catch (error) {
                 console.error("Error fetching loan outstanding report:", error);
                 res.send({ suc: 0, msg: "An error occurred" });
