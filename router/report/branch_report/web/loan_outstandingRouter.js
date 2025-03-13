7d70707357780e01d06edf915461edb8f0eca5fc
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-13 15:08:35 +0530

    start working on outstanding report 13.03.2025

--- router/report/branch_report/web/loan_outstandingRouter.js
+++ router/report/branch_report/web/loan_outstandingRouter.js
@@ -75,14 +75,14 @@ dateFormat = require('dateformat');
                 if (isCurrentDate) {
                     var select = "a.group_code,b.group_name,b.co_id,b.bank_name,b.acc_no1,b.acc_no2,a.recovery_day,a.prn_disb_amt,SUM(a.prn_amt + a.od_prn_amt) prn_outstanding,SUM(a.intt_amt) intt_outstanding,SUM(a.outstanding) outstanding,c.emp_name co_name",
                     table_name = "td_loan a LEFT JOIN md_group b ON a.group_code = b.group_code LEFT JOIN md_employee c ON b.co_id = c.emp_id",
-                    whr = null,
+                    whr = `a.branch_code = '${data.branch_code}'`,
                     order = `GROUP BY a.group_code,b.group_name,b.co_id,b.bank_name,b.acc_no1,b.acc_no2,a.recovery_day,a.prn_disb_amt,c.emp_name`;
                     var outstanding_data = await db_Select(select,table_name,whr,order);
                     res.send(outstanding_data);
                 }else {
                     var select = "b.group_code,c.group_name,c.co_id,c.bank_name,c.acc_no1,c.acc_no2,b.recovery_day,b.prn_disb_amt,SUM(a.prn_amt) prn_outstanding,SUM(a.intt_amt) intt_outstanding,SUM(a.outstanding) outstanding,d.emp_name co_name",
                     table_name = "td_loan_month_balance a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_group c ON b.group_code = c.group_code LEFT JOIN md_employee d ON c.co_id = d.emp_id",
-                    whr = `a.balance_date = '${data.supply_date}'`,
+                    whr = `a.balance_date = '${data.supply_date}' AND b.branch_code = '${data.branch_code}'`,
                     order = `GROUP BY b.group_code,c.group_name,c.co_id,c.bank_name,c.acc_no1,c.acc_no2,b.recovery_day,b.prn_disb_amt,d.emp_name`;
                     var outstanding_data = await db_Select(select,table_name,whr,order);
                     res.send(outstanding_data);
