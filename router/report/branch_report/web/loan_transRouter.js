7dbb364e501da82d9938b4643279b71722d94f04
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-18 10:57:09 +0530

    start working on transaction report17.03.2025

--- router/report/branch_report/web/loan_transRouter.js
+++ router/report/branch_report/web/loan_transRouter.js
@@ -64,8 +64,7 @@ loan_transRouter.post("/transaction_report_groupwise", async (req, res) => {
     var data = req.body;
  
     var select = `b.group_code,c.group_name,c.acc_no1,c.acc_no2,c.grp_addr,c.co_id,d.emp_name co_name,
-    SUM(${data.tr_type === 'D' ? 'a.debit' : '0'}) AS debit,
-    SUM(${data.tr_type === 'R' ? 'a.credit' : '0'}) AS credit`,
+      ${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'}`
     table_name = "td_loan_transactions a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_group c ON  b.group_code = c.group_code LEFT JOIN md_employee d ON c.co_id = d.emp_id",
     whr = `a.branch_id = '${data.branch_code}' AND a.payment_date BETWEEN '${data.from_dt}' AND '${data.to_dt}'
            AND a.tr_type = '${data.tr_type}'`,
@@ -85,7 +84,7 @@ loan_transRouter.post("/transaction_report_fundwise", async (req, res) => {
   try{
     var data = req.body;
  
-    var select = "b.group_code,c.group_name,b.fund_id,e.fund_name,c.acc_no1,c.acc_no2,c.grp_addr,c.co_id,d.emp_name co_name,SUM(a.debit)debit,SUM(a.credit)credit",
+    var select = `b.group_code,c.group_name,b.fund_id,e.fund_name,c.acc_no1,c.acc_no2,c.grp_addr,c.co_id,d.emp_name co_name,${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'}`,
     table_name = "td_loan_transactions a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_group c ON  b.group_code = c.group_code LEFT JOIN md_employee d ON c.co_id = d.emp_id LEFT JOIN md_fund e ON b.fund_id = e.fund_id",
     whr = `a.branch_id = '${data.branch_code}' AND a.payment_date BETWEEN '${data.from_dt}' AND '${data.to_dt}'
            AND b.fund_id = '${data.fund_id}' AND a.tr_type = '${data.tr_type}'`,
@@ -105,7 +104,7 @@ loan_transRouter.post("/transaction_report_cowise", async (req, res) => {
   try{
     var data = req.body;
  
-    var select = "c.co_id,d.emp_name co_name,COUNT(c.group_code) AS total_group,COUNT(b.member_code) AS total_member,b.fund_id,e.fund_name,SUM(a.debit)debit,SUM(a.credit)credit",
+    var select = `c.co_id,d.emp_name co_name,COUNT(c.group_code) AS total_group,COUNT(b.member_code) AS total_member,b.fund_id,e.fund_name,${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'}`,
     table_name = "td_loan_transactions a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_group c ON b.group_code = c.group_code LEFT JOIN md_employee d ON c.co_id = d.emp_id LEFT JOIN md_fund e ON b.fund_id = e.fund_id",
     whr = `a.branch_id = '${data.branch_code}' AND a.payment_date BETWEEN '${data.from_dt}' AND '${data.to_dt}'
            AND c.co_id = '${data.co_id}' AND a.tr_type = '${data.tr_type}'`,
@@ -124,7 +123,7 @@ loan_transRouter.post("/transaction_report_cowise", async (req, res) => {
     try{
       var data = req.body;
    
-      var select = "b.group_code,c.group_name,c.acc_no1,c.acc_no2,c.grp_addr,c.co_id,d.emp_name,b.loan_id,b.member_code,e.client_name,b.scheme_id,f.scheme_name,a.payment_id transaction_id,a.payment_date transaction_date,a.particulars,a.debit,a.credit,(a.balance + a.od_balance + a.intt_balance)balance,a.created_by created_code,a.created_at,g.emp_name created_by,a.approved_at,a.approved_by approved_code,h.emp_name approved_by",
+      var select = `b.group_code,c.group_name,c.acc_no1,c.acc_no2,c.grp_addr,c.co_id,d.emp_name,b.loan_id,b.member_code,e.client_name,b.scheme_id,f.scheme_name,a.payment_id transaction_id,a.payment_date transaction_date,a.particulars,${data.tr_type === 'D' ? 'a.debit AS debit' : 'a.credit AS credit'},(a.balance + a.od_balance + a.intt_balance)balance,a.created_by created_code,a.created_at,g.emp_name created_by,a.approved_at,a.approved_by approved_code,h.emp_name approved_by`,
       table_name = "td_loan_transactions a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_group c ON b.group_code = c.group_code LEFT JOIN md_employee d ON c.co_id = d.emp_id LEFT JOIN md_member e ON b.member_code = e.member_code LEFT JOIN md_scheme f ON b.scheme_id = f.scheme_id LEFT JOIN md_employee g ON a.created_by = g.emp_id LEFT JOIN md_employee h ON a.approved_by = h.emp_id",
       whr = `a.branch_id = '${data.branch_code}' AND a.payment_date BETWEEN '${data.from_dt}' AND '${data.to_dt}' AND a.tr_type = '${data.tr_type}'`,
       order = `ORDER BY payment_id desc, payment_date desc`;
