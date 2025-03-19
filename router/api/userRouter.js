10f6292e4a668d34505ad1a98dbd7cfb28d2fc9e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-19 11:47:57 +0530

    start working on single sign on 19.03.2025

--- router/api/userRouter.js
+++ router/api/userRouter.js
@@ -135,7 +135,7 @@ userRouter.post('/logout', async(req, res) => {
    const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
    var table_name = "md_user",
-   fields = `refresh_token = NULL, modified_by = '${data.modified_by}', modified_at = '${datetime}'`
+   fields = `refresh_token = NULL, session_id = NULL, modified_by = '${data.modified_by}', modified_at = '${datetime}'`
    whr = `emp_id = '${data.emp_id}' AND session_id = '${data.session_id}'`,
    flag = 1;
   var del_ref_token = await db_Insert(table_name,fields,whr,flag);
