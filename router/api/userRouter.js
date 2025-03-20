8d6e00ae9c585d445a2cc5072308868f42dbad37
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-20 18:18:22 +0530

    commit refresh token 20.03.2025

--- router/api/userRouter.js
+++ router/api/userRouter.js
@@ -343,7 +343,7 @@ try{
 // Middleware for Authentication
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers['Authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) return res.json({ error: 'Token is required' });
