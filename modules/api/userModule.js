09e96fd436e7db3bc24d7adf239d475bd1ef647a
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-20 15:24:30 +0530

    start working on single sign on 19.03.2025

--- modules/api/userModule.js
+++ modules/api/userModule.js
@@ -3,7 +3,8 @@ const { db_Select } = require("../../model/mysqlModel");
 
 module.exports = {
     app_login_data : (data) => {
-        return new Promise(async (resolve, reject) => { var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type, a.session_id,a.refresh_token,c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
+        return new Promise(async (resolve, reject) => { 
+          var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type, a.session_id,a.refresh_token,c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
           table_name = "md_user a, md_user_type b,  md_employee c, md_branch d",
           whr = `a.emp_id=c.emp_id
           AND  a.user_type=b.type_code
