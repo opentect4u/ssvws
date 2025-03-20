78d373e4ebf02b66853cf1b3b5525257acfefe05
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-20 15:23:26 +0530

    start working on single sign on 19.03.2025

--- modules/api/userModule.js
+++ modules/api/userModule.js
@@ -3,19 +3,7 @@ const { db_Select } = require("../../model/mysqlModel");
 
 module.exports = {
     app_login_data : (data) => {
-        return new Promise(async (resolve, reject) => {
-          // var select = "a.emp_id,a.brn_code, a.user_type, a.password, b.user_type, c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, d.branch_name, d.dist_code, c.area_code, e.area_name, f.state_code, f.dist_id, f.block_id, f.zone_name",
-          // table_name = "md_user a, md_user_type b,  md_employee c, md_branch d, md_area e, md_zone f",
-          // whr = `a.emp_id=c.emp_id
-          // AND  a.user_type=b.type_code
-          // AND a.brn_code=d.branch_code
-          // AND c.area_code=e.area_code 
-          // AND e.zone_code=f.zone_code
-          // AND a.emp_id = '${data.emp_id}'
-          // AND a.user_status = 'A'`
-          // order = null;
-
-          var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type, a.session_id,a.refresh_token,c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
+        return new Promise(async (resolve, reject) => { var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type, a.session_id,a.refresh_token,c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
           table_name = "md_user a, md_user_type b,  md_employee c, md_branch d",
           whr = `a.emp_id=c.emp_id
           AND  a.user_type=b.type_code
@@ -27,21 +15,5 @@ module.exports = {
         resolve(login_dt);
         });
 },
-
-// bm_login_data : (data) => {
-//   return new Promise(async (resolve, reject) => {
-
-//     var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type, c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
-//     table_name = "md_user a, md_user_type b,  md_employee c, md_branch d",
-//     whr = `a.emp_id=c.emp_id
-//     AND  a.user_type=b.type_code
-//     AND a.brn_code=d.branch_code
-//     AND a.emp_id = '${data.emp_id}'
-//     AND a.user_status = 'A'`
-//     order = null;
-//   var bm_login_dt = await db_Select(select, table_name, whr, order);
-//   resolve(bm_login_dt);
-//   });
-// }
 
 }
