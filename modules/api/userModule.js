e1d7ae17152f432448dfbc0fc9994aaca06f46c7
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-20 15:19:46 +0530

    start working on single sign on 19.03.2025

--- modules/api/userModule.js
+++ modules/api/userModule.js
@@ -14,17 +14,30 @@ module.exports = {
           // AND a.emp_id = '${data.emp_id}'
           // AND a.user_status = 'A'`
           // order = null;
-
-          var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type, a.session_id,a.refresh_token,c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
-          table_name = "md_user a, md_user_type b,  md_employee c, md_branch d",
-          whr = `a.emp_id=c.emp_id
-          AND  a.user_type=b.type_code
-          AND a.brn_code=d.branch_code
-          AND a.emp_id = '${data.emp_id}'
-          AND a.user_status = 'A'`
-          order = null;
-        var login_dt = await db_Select(select, table_name, whr, order);
-        resolve(login_dt);
+          
+          if(data.flag == 'A'){
+            var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type,c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
+            table_name = "md_user a, md_user_type b,  md_employee c, md_branch d",
+            whr = `a.emp_id=c.emp_id
+            AND  a.user_type=b.type_code
+            AND a.brn_code=d.branch_code
+            AND a.emp_id = '${data.emp_id}'
+            AND a.user_status = 'A'`
+            order = null;
+          var login_dt = await db_Select(select, table_name, whr, order);
+          resolve(login_dt);
+          }else {
+            var select = "a.emp_id,a.brn_code, a.user_type id, a.password, b.user_type, a.session_id,a.refresh_token,c.emp_name, c.phone_home, c.phone_mobile, c.email, c.gender, c.active_flag, c.area_code, d.branch_name, d.dist_code",
+            table_name = "md_user a, md_user_type b,  md_employee c, md_branch d",
+            whr = `a.emp_id=c.emp_id
+            AND  a.user_type=b.type_code
+            AND a.brn_code=d.branch_code
+            AND a.emp_id = '${data.emp_id}'
+            AND a.user_status = 'A'`
+            order = null;
+          var login_dt = await db_Select(select, table_name, whr, order);
+          resolve(login_dt);
+          }
         });
 },
 
