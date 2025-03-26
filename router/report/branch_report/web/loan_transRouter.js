d3f7949ca8d9a531a99d8ae3fac8520b7333b789
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-26 16:41:26 +0530

    start work on transaction report branchwise 26.03.2025

--- router/report/branch_report/web/loan_transRouter.js
+++ router/report/branch_report/web/loan_transRouter.js
@@ -142,7 +142,7 @@ loan_transRouter.post("/transaction_report_cowise", async (req, res) => {
     try{
       var data = req.body;
    
-      var select = `a.branch_id,c.branch_name,${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'}`,
+      var select = `a.branch_id,c.branch_name,${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'},SUM(b.prn_amt + b.od_prn_amt + b.intt_amt)curr_balance`,
       table_name = "td_loan_transactions a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_branch c ON a.branch_id = c.branch_code",
       whr = `a.branch_id = '${data.branch_code}' AND a.payment_date BETWEEN '${data.from_dt}' AND '${data.to_dt}' AND a.tr_type = '${data.tr_type}'`,
       order = `GROUP BY a.branch_id,c.branch_name`;
