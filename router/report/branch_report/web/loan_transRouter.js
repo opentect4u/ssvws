aa72904ebe64acd8f201285d99c726773989a870
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-26 16:38:11 +0530

    start work on transaction report branchwise 26.03.2025

--- router/report/branch_report/web/loan_transRouter.js
+++ router/report/branch_report/web/loan_transRouter.js
@@ -145,11 +145,11 @@ loan_transRouter.post("/transaction_report_cowise", async (req, res) => {
       var select = `a.branch_id,c.branch_name,${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'}`,
       table_name = "td_loan_transactions a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_branch c ON a.branch_id = c.branch_code",
       whr = `a.branch_id = '${data.branch_code}' AND a.payment_date BETWEEN '${data.from_dt}' AND '${data.to_dt}' AND a.tr_type = '${data.tr_type}'`,
-      order = `ORDER BY a.branch_id,c.branch_name`;
-      var transaction_member_data = await db_Select(select,table_name,whr,order);
-      res.send({transaction_member_data})
+      order = `GROUP BY a.branch_id,c.branch_name`;
+      var transaction_branch_data = await db_Select(select,table_name,whr,order);
+      res.send({transaction_branch_data})
     }catch (error){
-     console.error("Error fetching transaction report memberwise:", error);
+     console.error("Error fetching transaction report branchwise:", error);
      res.send({ suc: 0, msg: "An error occurred" });
     }
    });
