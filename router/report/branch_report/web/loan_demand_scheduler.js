d5e89dc4210d456bf7d1c79d6f42e33ed9922cc8
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-03-27 11:57:55 +0530

    commit loan_balance_scheduler 27.03.2025

--- router/report/branch_report/web/loan_demand_scheduler.js
+++ router/report/branch_report/web/loan_demand_scheduler.js
@@ -1,7 +1,48 @@
-const { default: dateFormat } = require('dateformat');
+const { db_Select } = require('../../../../model/mysqlModel');
 
 const express = require('express'),
 loan_demand_scheduler = express.Router(),
-dateFormat = require(dateFormat);
+dateFormat = require('dateformat');
+
+loan_demand_scheduler.post("/loan_demand_scheduler", async (req, res) => {
+    try {
+        var data = req.body;
+
+        var select = "branch_code,closed_upto",
+        table_name = "td_month_close",
+        whr = `demand_flag = 'N'`,
+        order = null;
+        var data_branch_demand = await db_Select(select, table_name, whr, order);
+
+        if (data_branch_demand.suc > 0 && data_branch_demand.msg.length > 0) {
+            var branch_codes = data_branch_demand.msg.map(item => item.branch_code);
+            var closed_uptos = data_branch_demand.msg.map(item => item.closed_upto);
+            let demandData = [];
+            console.log(branch_codes,closed_uptos,'demandData');
+            
+
+            for (let branch_code of branch_codes) {
+                let select = "branch_code,loan_id",
+                    table_name = "td_loan",
+                    whr = `branch_code = '${branch_code}' AND outstanding > 0`,
+                    order = null;
+                    var data_demand = await db_Select(select, table_name, whr, order);
+                
+                if (data_demand.suc > 0 && data_demand.msg.length > 0) {
+                    demandData.push(...data_demand.msg);
+                }
+            }
+            if (demandData.length === 0) {
+                return res.json({ success: false, message: "No loans found with outstanding balance" });
+            }
+            return res.json({ success: true, data: demandData }); 
+        }else {
+            return res.json({ success: false, message: "No branches found" });
+        }
+    }catch (error) {
+        console.error("Error:", error);
+        return res.json({ success: false, error: "Internal Server Error" });
+    }
+});
 
 module.exports = {loan_demand_scheduler}
