aa54f1c3892d7cb11ba475b982b15229940e6587
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-04-07 11:36:01 +0530

    commit demand_vs_collection_report rectification 07.04.2025_latest

--- router/report/branch_report/web/dmd_vs_collRouter.js
+++ router/report/branch_report/web/dmd_vs_collRouter.js
@@ -90,7 +90,7 @@ dmd_vs_collRouter.post("/dmd_vs_collec_report_groupwise", async (req, res) => {
    var create_date = dateFormat(dateResult.msg[0].month_last_date,'yyyy-mm-dd');
    var first_create_date = dateFormat(first_dateResult.msg[0].first_day_of_month,'yyyy-mm-dd');
 
-   var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,MAX(b.disb_dt)disb_dt,SUM(b.prn_disb_amt)disb_amt,b.curr_roi,b.period,b.period_mode, 
+   var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,CONCAT(DATE_FORMAT(STR_TO_DATE('${first_create_date}', '%Y-%m-%d'), '%M %Y'), ' to ', DATE_FORMAT(STR_TO_DATE('${create_date}', '%Y-%m-%d'), '%M %Y')) AS collec_upto,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,MAX(b.disb_dt)disb_dt,SUM(b.prn_disb_amt)disb_amt,b.curr_roi,b.period,b.period_mode, 
         CASE 
         WHEN b.period_mode = 'Monthly' THEN b.recovery_day
         WHEN b.period_mode = 'Weekly' THEN 
@@ -107,8 +107,7 @@ dmd_vs_collRouter.post("/dmd_vs_collec_report_groupwise", async (req, res) => {
         ELSE 'N/A'
         END AS recovery_day,b.instl_start_dt,b.instl_end_dt,SUM(b.tot_emi)tot_emi,SUM(a.dmd_amt)demand_amt,SUM(f.credit)collection_amt,SUM(a.dmd_amt) - SUM(f.credit),SUM(b.outstanding)curr_outstanding`,
    table_name = "td_loan_month_demand a LEFT JOIN td_loan b ON a.branch_code = b.branch_code AND a.loan_id = b.loan_id LEFT JOIN md_branch c ON a.branch_code = c.branch_code LEFT JOIN md_group d ON b.group_code = d.group_code LEFT JOIN md_employee e ON d.co_id = e.emp_id LEFT JOIN td_loan_transactions f ON a.loan_id = f.loan_id",
-   whr = `a.branch_code IN (${data.branch_code}) AND a.demand_date = '${create_date}'
-          AND f.payment_date BETWEEN '${first_create_date}' AND '${create_date}'`,
+   whr = `a.branch_code IN (${data.branch_code}) AND a.demand_date BETWEEN '${first_create_date}' AND '${create_date}' AND f.payment_date BETWEEN '${first_create_date}' AND '${create_date}'`,
    order = `GROUP BY a.demand_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name,b.curr_roi,b.period,b.period_mode,b.instl_start_dt,b.instl_end_dt`;
    var groupwise_demand_collec_data = await db_Select(select,table_name,whr,order)
    res.send({groupwise_demand_collec_data, dateRange: `BETWEEN '${first_create_date}' AND '${create_date}'`});
@@ -136,7 +135,7 @@ dmd_vs_collRouter.post("/dmd_vs_collec_report_fundwise", async (req, res) => {
     var create_date = dateFormat(dateResult.msg[0].month_last_date,'yyyy-mm-dd');
     var first_create_date = dateFormat(first_dateResult.msg[0].first_day_of_month,'yyyy-mm-dd');
 
-    var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,b.fund_id,f.fund_name,b.period_mode, 
+    var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,CONCAT(DATE_FORMAT(STR_TO_DATE('${first_create_date}', '%Y-%m-%d'), '%M %Y'), ' to ', DATE_FORMAT(STR_TO_DATE('${create_date}', '%Y-%m-%d'), '%M %Y')) AS collec_upto,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,b.fund_id,f.fund_name,b.period_mode, 
         CASE 
         WHEN b.period_mode = 'Monthly' THEN b.recovery_day
         WHEN b.period_mode = 'Weekly' THEN 
@@ -193,7 +192,7 @@ dmd_vs_collRouter.post("/dmd_vs_collec_report_cowise", async (req, res) => {
     var create_date = dateFormat(dateResult.msg[0].month_last_date,'yyyy-mm-dd');
     var first_create_date = dateFormat(first_dateResult.msg[0].first_day_of_month,'yyyy-mm-dd');
 
-    var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,b.period_mode, 
+    var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,CONCAT(DATE_FORMAT(STR_TO_DATE('${first_create_date}', '%Y-%m-%d'), '%M %Y'), ' to ', DATE_FORMAT(STR_TO_DATE('${create_date}', '%Y-%m-%d'), '%M %Y')) AS collec_upto,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,b.period_mode, 
         CASE 
         WHEN b.period_mode = 'Monthly' THEN b.recovery_day
         WHEN b.period_mode = 'Weekly' THEN 
@@ -237,7 +236,7 @@ dmd_vs_collRouter.post("/dmd_vs_collec_report_memberwise", async (req, res) => {
     var create_date = dateFormat(dateResult.msg[0].month_last_date,'yyyy-mm-dd');
     var first_create_date = dateFormat(first_dateResult.msg[0].first_day_of_month,'yyyy-mm-dd');
 
-    var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,a.branch_code,c.branch_name,b.loan_id,b.member_code,f.client_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,b.disb_dt,b.prn_disb_amt disb_amt,b.curr_roi,b.period,b.period_mode,CASE 
+    var select = `DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,CONCAT(DATE_FORMAT(STR_TO_DATE('${first_create_date}', '%Y-%m-%d'), '%M %Y'), ' to ', DATE_FORMAT(STR_TO_DATE('${create_date}', '%Y-%m-%d'), '%M %Y')) AS collec_upto,a.branch_code,c.branch_name,b.loan_id,b.member_code,f.client_name,b.group_code,d.group_name,d.co_id,e.emp_name co_name,b.disb_dt,b.prn_disb_amt disb_amt,b.curr_roi,b.period,b.period_mode,CASE 
         WHEN b.period_mode = 'Monthly' THEN b.recovery_day
         WHEN b.period_mode = 'Weekly' THEN 
         CASE b.recovery_day
@@ -280,7 +279,7 @@ dmd_vs_collRouter.post("/dmd_vs_collec_report_branchwise", async (req, res) => {
         var create_date = dateFormat(dateResult.msg[0].month_last_date,'yyyy-mm-dd');
         var first_create_date = dateFormat(first_dateResult.msg[0].first_day_of_month,'yyyy-mm-dd');
 
-        var select = "DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,a.branch_code,c.branch_name,SUM(b.tot_emi) total_ami,SUM(a.dmd_amt) demand_amt,SUM(d.credit) collection_amt,SUM(a.dmd_amt) - SUM(d.credit),SUM(b.outstanding) curr_outstanding",
+        var select = "DATE_FORMAT(a.demand_date, '%M %Y') AS demand_date,CONCAT(DATE_FORMAT(STR_TO_DATE('${first_create_date}', '%Y-%m-%d'), '%M %Y'), ' to ', DATE_FORMAT(STR_TO_DATE('${create_date}', '%Y-%m-%d'), '%M %Y')) AS collec_upto,a.branch_code,c.branch_name,SUM(b.tot_emi) total_ami,SUM(a.dmd_amt) demand_amt,SUM(d.credit) collection_amt,SUM(a.dmd_amt) - SUM(d.credit),SUM(b.outstanding) curr_outstanding",
         table_name = "td_loan_month_demand a LEFT JOIN td_loan b ON a.branch_code = b.branch_code AND a.loan_id = b.loan_id LEFT JOIN md_branch c ON a.branch_code = c.branch_code LEFT JOIN td_loan_transactions d ON a.loan_id = b.loan_id",
         whr = `a.branch_code IN (${data.branch_code}) AND a.demand_date = '${create_date}' AND d.payment_date BETWEEN '${first_create_date}' AND '${create_date}'`,
         order = `GROUP BY a.demand_date,a.branch_code,c.branch_name`;
