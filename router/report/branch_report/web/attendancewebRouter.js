ec20c2e5ec4ad7e1a060569ecd80beebf4f1ac8c
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-04-09 16:35:34 +0530

    commit add checking  recovery transaction 09.04.2025

--- router/report/branch_report/web/attendancewebRouter.js
+++ router/report/branch_report/web/attendancewebRouter.js
@@ -9,13 +9,21 @@ attendancewebRouter.post("/fetch_employee_fr_branch", async (req, res) => {
     try{
         var data = req.body;
         //fetch employee name through branch_id
-        var select = "emp_id,branch_id,emp_name",
-        table_name = "md_employee",
-        whr = `branch_id = '${data.branch_id}'`,
-        order = null;
-        var emp_dtls_web = await db_Select(select,table_name,whr,order);
-        res.send(emp_dtls_web)
-
+        if(data.branch_id == 'A'){
+            var select = "emp_id,branch_id,emp_name",
+            table_name = "md_employee",
+            whr = null,
+            order = null;
+            var emp_dtls_web = await db_Select(select,table_name,whr,order);
+            res.send(emp_dtls_web)
+        }else {
+            var select = "emp_id,branch_id,emp_name",
+            table_name = "md_employee",
+            whr = `branch_id = '${data.branch_id}'`,
+            order = null;
+            var emp_dtls_web = await db_Select(select,table_name,whr,order);
+            res.send(emp_dtls_web)
+        }
     } catch (error) {
         console.error("Error fetching employee details:", error);
         res.send({ suc: 0, msg: "An error occurred" });
