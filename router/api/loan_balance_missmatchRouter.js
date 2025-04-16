fa5f28e8bb89e085d9e6b80291d1b075e4248b24
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-04-16 11:30:46 +0530

    commit write api to fetch wrong balance 16.04.2025

--- router/api/loan_balance_missmatchRouter.js
+++ router/api/loan_balance_missmatchRouter.js
@@ -11,7 +11,7 @@ loan_balance_missmatchRouter.get("/fetch_loan_missmatch_balance", async (req, re
         const currentDate = dateFormat(new Date(), "yyyy-mm-dd");
         // console.log(currentDate,'curre');
         var balance_missmatch = await db_Select(null,null,null,null,true,`CALL p_loan_balance_mismatch('${currentDate}')`);
-        res.json(balance_missmatch) 
+        res.json({success: true , balance_missmatch}) ;
     } catch (error) {
         console.error("Error fetching loan mismatch balance:", error);
         res.json({ error: "Server error", details: error.message });
