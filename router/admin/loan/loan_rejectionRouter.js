2dff20dac2fcf0066227868a6d14738c2d40ccb1
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-04-16 16:49:48 +0530

    commit rejection of loan transactions 16.04.2025

--- router/admin/loan/loan_rejectionRouter.js
+++ router/admin/loan/loan_rejectionRouter.js
@@ -9,9 +9,9 @@ loan_rejectionRouter.post("/reject_loan_transactions", async (req, res) => {
         var data = req.body;
 
         //fetch loan id from td_loan table
-        var select = "a.loan_id",
-        table_name = "td_loan a LEFT JOIN md_group b ON a.group_code = b.group_code",
-        whr = `a.group_code like '%${data.grp}%' OR b.group_name like '%${data.grp}%'`,
+        var select = "loan_id",
+        table_name = "td_loan",
+        whr = `branch_code = '${data.branch_code}' AND group_code = '%${data.group_code}%'`,
         order = null;
         var loan_details = await db_Select(select,table_name,whr,order);
         res.send(loan_details)
