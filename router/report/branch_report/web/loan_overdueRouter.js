a8fe30a37dda20f1fc259c7f668860e875d5ac88
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-04-17 17:27:20 +0530

    commit overdue report 17.04.2025

--- router/report/branch_report/web/loan_overdueRouter.js
+++ router/report/branch_report/web/loan_overdueRouter.js
@@ -42,10 +42,10 @@ loan_overdueRouter.post("/fetch_usertypeWise_branch_name", async (req, res) => {
           }
 
             for(let dt of data.search_brn_id){
-            var select = "a.trf_date,a.od_date first_od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id code,e.emp_name co_id,b.recovery_day,b.disb_dt,a.disb_amt,b.instl_end_dt,b.period,b.period_mode,a.od_amt,SUM(a.outstanding) outstanding",
+            var select = "a.trf_date,a.od_date first_od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id code,e.emp_name co_id,b.recovery_day,b.disb_dt,SUM(a.disb_amt)disb_amt,b.instl_end_dt,b.period,b.period_mode,SUM(a.od_amt)od_amt,SUM(a.outstanding) outstanding",
             table_name = "td_od_loan a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_branch c ON a.branch_code = c.branch_code LEFT JOIN md_group d ON b.group_code = d.group_code LEFT JOIN md_employee e ON d.co_id = e.emp_id",
             whr = `a.branch_code IN (${dt.branch_code}) AND a.trf_date = (SELECT MAX(trf_date) FROM td_od_loan WHERE branch_code IN (${dt.branch_code}) AND trf_date <= '${data.send_date}')`,
-            order = `GROUP BY a.trf_date,a.od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name,b.recovery_day,b.disb_dt,a.disb_amt,b.instl_end_dt,b.period,b.period_mode,a.od_amt
+            order = `GROUP BY a.trf_date,a.od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name,b.recovery_day,b.disb_dt,b.instl_end_dt,b.period,b.period_mode
             ORDER BY b.group_code`;
             var loan_overdue_dtls = await db_Select(select, table_name, whr, order);
 
@@ -70,12 +70,12 @@ loan_overdueRouter.post("/fetch_usertypeWise_branch_name", async (req, res) => {
               }
 
             for(let dt of data.search_brn_id){
-           var select = "a.trf_date,a.od_date first_od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id code,e.emp_name co_id,b.fund_id,f.fund_name,b.recovery_day,b.disb_dt,a.disb_amt,b.instl_end_dt,b.period,b.period_mode,a.od_amt,SUM(a.outstanding) outstanding",
+           var select = "a.trf_date,a.od_date first_od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id code,e.emp_name co_id,b.fund_id,f.fund_name,b.recovery_day,b.disb_dt,SUM(a.disb_amt)disb_amt,b.instl_end_dt,b.period,b.period_mode,SUM(a.od_amt)od_amt,SUM(a.outstanding) outstanding",
           table_name = "td_od_loan a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_branch c ON a.branch_code = c.branch_code LEFT JOIN md_group d ON b.group_code = d.group_code LEFT JOIN md_employee e ON d.co_id = e.emp_id LEFT JOIN md_fund f ON b.fund_id = f.fund_id",
           whr = `a.branch_code IN (${dt.branch_code}) AND a.trf_date = (SELECT MAX(trf_date) FROM td_od_loan
                                                                            WHERE branch_code IN (${dt.branch_code})
                                                                            AND trf_date <= '${data.send_date}') AND b.fund_id = '${data.fund_id}'`,
-          order = `GROUP BY a.trf_date,a.od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name,b.fund_id,f.fund_name,b.recovery_day,b.disb_dt,a.disb_amt,b.instl_end_dt,b.period,b.period_mode,a.od_amt
+          order = `GROUP BY a.trf_date,a.od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name,b.fund_id,f.fund_name,b.recovery_day,b.disb_dt,b.instl_end_dt,b.period,b.period_mode,
           ORDER BY b.group_code`;
           var loan_overdue_dtls_fundwise = await db_Select(select, table_name, whr, order);
           finalData.push(...loan_overdue_dtls_fundwise.msg)
@@ -100,12 +100,12 @@ loan_overdueRouter.post("/fetch_usertypeWise_branch_name", async (req, res) => {
               }
 
               for(let dt of data.search_brn_id){
-           var select = "a.trf_date,a.od_date first_od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id code,e.emp_name co_id,b.recovery_day,b.disb_dt,a.disb_amt,b.instl_end_dt,b.period,b.period_mode,a.od_amt,SUM(a.outstanding) outstanding",
+           var select = "a.trf_date,a.od_date first_od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id code,e.emp_name co_id,b.recovery_day,b.disb_dt,SUM(a.disb_amt)disb_amt,b.instl_end_dt,b.period,b.period_mode,SUM(a.od_amt)od_amt,SUM(a.outstanding) outstanding",
           table_name = "td_od_loan a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_branch c ON a.branch_code = c.branch_code LEFT JOIN md_group d ON b.group_code = d.group_code LEFT JOIN md_employee e ON d.co_id = e.emp_id",
           whr = `a.branch_code IN (${dt.branch_code}) AND a.trf_date = (SELECT MAX(trf_date) FROM td_od_loan
                                                                            WHERE branch_code IN (${dt.branch_code})
                                                                            AND trf_date <= '${data.send_date}') AND d.co_id IN (${data.co_id})`,
-          order = `GROUP BY a.trf_date,a.od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name,b.recovery_day,b.disb_dt,a.disb_amt,b.instl_end_dt,b.period,b.period_mode,a.od_amt
+          order = `GROUP BY a.trf_date,a.od_date,a.branch_code,c.branch_name,b.group_code,d.group_name,d.co_id,e.emp_name,b.recovery_day,b.disb_dt,b.instl_end_dt,b.period,b.period_mode
           ORDER BY b.group_code`;
           var loan_overdue_dtls_cowise = await db_Select(select, table_name, whr, order);
           finalData.push(...loan_overdue_dtls_cowise.msg)
