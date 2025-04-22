b04bc13b8d28f14e84b3ad1be4fd1acf9ee5e591
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-04-22 16:27:11 +0530

    rectify approve recovery section 22.04.2025

--- router/admin/loan/loan_recov_approve.js
+++ router/admin/loan/loan_recov_approve.js
@@ -107,7 +107,7 @@ loan_recov_approveRouter.post("/checking_before_approve", async (req, res) => {
                 var loan_ids = loan_id_dt.msg.map(dts => dts.loan_id).join(",");
                 var select = "COUNT(*) tot_row",
                 table_name = "td_loan_transactions",
-                whr = `loan_id = '${loan_ids}' AND payment_date <= '${dateFormat(dt.payment_date,'yyyy-mm-dd')}' AND status = 'U'`
+                whr = `loan_id IN (${loan_ids}) AND payment_date <= '${dateFormat(dt.payment_date,'yyyy-mm-dd')}' AND status = 'U'`
                 order = null;
                 var check_dt = await db_Select(select,table_name,whr,order);
             }
