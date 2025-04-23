00c47ffdf84eb306d40bb6c005715d44341b2ada
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-04-23 16:13:52 +0530

    write api to update recovery day 23.04.2025

--- router/admin/loanRouter.js
+++ router/admin/loanRouter.js
@@ -690,6 +690,8 @@ loanRouter.post("/change_recovery_day", async (req, res) => {
    let datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 
   for (let dt of data.recov_day_dtls){
+    console.log(dt,'dtttt');
+    
     var table_name = "td_loan",
     fields = `recovery_day = '${data.recovery_day}', modified_by = '${data.modified_by}', modified_dt = '${datetime}'`,
     values = null,
