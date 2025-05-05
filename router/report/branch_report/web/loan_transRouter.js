8c2413bc4c434ecdaa317d0c28280ba09209ec1e
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-05-05 17:11:27 +0530

    commit rectify loan statement report 03.05.2025

--- router/report/branch_report/web/loan_transRouter.js
+++ router/report/branch_report/web/loan_transRouter.js
@@ -91,7 +91,7 @@ loan_transRouter.post("/transaction_report_groupwise", async (req, res) => {
     var data = req.body;
  
     var select = `b.branch_code,e.branch_name,a.payment_date,b.group_code,c.group_name,c.acc_no1,c.acc_no2,c.grp_addr,c.co_id,d.emp_name co_name,
-      ${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'}`
+      ${data.tr_type === 'D' ? 'SUM(a.debit) AS debit' : 'SUM(a.credit) AS credit'},SUM(b.prn_amt + b.od_prn_amt + b.intt_amt)curr_balance`
     table_name = "td_loan_transactions a LEFT JOIN td_loan b ON a.loan_id = b.loan_id LEFT JOIN md_group c ON  b.group_code = c.group_code LEFT JOIN md_employee d ON c.co_id = d.emp_id LEFT JOIN md_branch e ON b.branch_code = e.branch_code",
     whr = `a.branch_id IN (${data.branch_code}) AND a.payment_date BETWEEN '${data.from_dt}' AND '${data.to_dt}'
            AND a.tr_type = '${data.tr_type}'`,
