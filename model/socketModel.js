4ba781f42d7be9f6979bd5f6eabc7afd2f3c793b
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-10-07 12:39:50 +0530

    rectify loan transaction report make it as old version 07.10.2025

--- model/socketModel.js
+++ model/socketModel.js
@@ -5,7 +5,8 @@ let io;
 function initSocket(server) {
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:3000",
+            // origin: "http://localhost:3000",
+            origin: "https://ssvws.opentech4u.co.in",
             methods: ["GET", "POST"]
         }
     });
