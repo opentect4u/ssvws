771d8b8bc8d7adfb7a37ce6df551c2af3cb5c45c
Sayantika Dhar <sayantika.dhar04@gmail.com>
2025-10-08 18:29:26 +0530

    commit change dashboard demand section 08.10.2025

--- router/admin/dashboard/dashboard_dataRouter.js
+++ router/admin/dashboard/dashboard_dataRouter.js
@@ -1012,11 +1012,11 @@ dashboard_dataRouter.post("/dashboard_demand_amt_fr_allbrn", async (req, res) =>
 
     const result = {
       total_loan_dmd: 0,
-      total_dmd_groups: 0,
+      total_demand_groups: 0,
       weekly_loan_dmd: 0,
-      weekly_dmd_groups: 0,
+      weekly_demand_groups: 0,
       monthly_loan_dmd: 0,
-      monthly_dmd_groups: 0,
+      monthly_demand_groups: 0,
     };
     
     const branchDateMap = {};
@@ -1038,7 +1038,7 @@ dashboard_dataRouter.post("/dashboard_demand_amt_fr_allbrn", async (req, res) =>
       );
 
         result.total_loan_dmd += Number(totalLoanDmd.msg[0].tot_loan_Dmd) || 0;
-        result.total_dmd_groups += Number(totalLoanDmd.msg[0].tot_demand_grp) || 0;
+        result.total_demand_groups += Number(totalLoanDmd.msg[0].tot_demand_grp) || 0;
 
       } else if (data.flag === 'W') {
        weeklyLoanDmd = await db_Select(
@@ -1054,7 +1054,7 @@ dashboard_dataRouter.post("/dashboard_demand_amt_fr_allbrn", async (req, res) =>
       );
 
         result.weekly_loan_dmd += Number(weeklyLoanDmd.msg[0].weekly_Dmd) || 0;
-        result.weekly_dmd_groups += Number(weeklyLoanDmd.msg[0].weekly_demand_grp) || 0;
+        result.weekly_demand_groups += Number(weeklyLoanDmd.msg[0].weekly_demand_grp) || 0;
 
       } else {
          monthlyLoanDmd = await db_Select(
@@ -1069,7 +1069,7 @@ dashboard_dataRouter.post("/dashboard_demand_amt_fr_allbrn", async (req, res) =>
         null
       );
         result.monthly_loan_dmd += Number(monthlyLoanDmd.msg[0].monthly_Dmd) || 0;
-        result.monthly_dmd_groups += Number(monthlyLoanDmd.msg[0].monthly_demand_grp) || 0;
+        result.monthly_demand_groups += Number(monthlyLoanDmd.msg[0].monthly_demand_grp) || 0;
 
       }
 
